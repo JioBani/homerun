68777bb15cf4854314da7c09369a883583b6d2b1
DESKTOP-4TIMLA2\Dev <spdlqj8876@naver.com>
2023-08-21 20:46:00 +0900

    예외 처리 및 로그 추가

--- lib/Service/SurveyDataSaveService.dart
+++ lib/Service/SurveyDataSaveService.dart
@@ -1,8 +1,10 @@
+import 'package:logger/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SurveyDataSaveService{
 
   static SurveyDataSaveService? _instance;
+  Logger _logger = Logger();
 
   SurveyDataSaveService._();
 
@@ -13,12 +15,34 @@ class SurveyDataSaveService{
   }
 
   Future<void> saveData(String key, String value) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString(key, value);
+    try{
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString(key, value);
+      _logger.log(Level.info, "데이터 저장 성공 : {${key} : ${value}}");
+    }
+    catch(e){
+      _logger.e(e);
+    }
   }
 
   Future<String> loadData(String key) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString(key) ?? "";
+    try{
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? result = prefs.getString(key);
+      if(result == null){
+        _logger.log(Level.warning, "키가 없음 : ${key}");
+        return "";
+      }
+      else{
+        _logger.log(Level.info, "데이터 불러오기 성공 : {${key} : ${result}}");
+        return result;
+      }
+    }
+    catch(e){
+      _logger.e(e);
+      return "";
+    }
+
+
   }
 }
