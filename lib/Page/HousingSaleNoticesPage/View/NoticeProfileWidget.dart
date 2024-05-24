02a934828b9db4857137b41c858327c0026ff03f
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-05-24 20:44:36 +0900

    Feat : 컨테이너 추가

---
+++ lib/Page/HousingSaleNoticesPage/View/NoticeProfileWidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class NoticeProfileWidget extends StatelessWidget {
+  const NoticeProfileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120.w,
+      margin: EdgeInsets.all(5.0),
+      color: Colors.red,
+    );
+  }
+}
