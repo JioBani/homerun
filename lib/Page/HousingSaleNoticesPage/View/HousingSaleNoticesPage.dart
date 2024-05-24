db49892b632f2e3dc3549ba12db6a799966ea161
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-05-24 16:49:19 +0900

    Stateless 위젯 생성

---
+++ lib/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class HousingSaleNoticesPage extends StatelessWidget {
+  const HousingSaleNoticesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
