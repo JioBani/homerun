d4370f2d7922575c09579caf1227048b2b9c6641
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-05-24 18:03:21 +0900

    칼럼 추

--- lib/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart
+++ lib/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HousingSaleNoticesPage extends StatelessWidget {
@@ -5,6 +6,14 @@ class HousingSaleNoticesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
   }
 }
