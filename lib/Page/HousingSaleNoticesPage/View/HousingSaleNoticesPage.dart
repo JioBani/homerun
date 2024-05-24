6aa380fb47ee386a581e73685c4d59ea30ffef76
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-05-24 20:42:25 +0900

    Feat : 칼럼에 패딩 추가

--- lib/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart
+++ lib/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart
@@ -13,44 +13,47 @@ class HousingSaleNoticesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Center(
-              child: Text("청약홈런",
-                style: TextStyle(
-                    fontSize: 30.sp,
-                    fontWeight: FontWeight.w600,
-                    color: Palette.defaultBlue
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(25.w, 0, 25.w, 0),
+          child: Column(
+            children: [
+              Center(
+                child: Text("청약홈런",
+                  style: TextStyle(
+                      fontSize: 30.sp,
+                      fontWeight: FontWeight.w600,
+                      color: Palette.defaultBlue
+                  ),
                 ),
               ),
-            ),
-            CustomSearchBar(),
-            DefaultTabController(
-                length: 3,
-                child: Expanded(
-                  child: Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.r),
-                            color: const Color.fromRGBO(251, 251, 251, 1)
+              CustomSearchBar(),
+              DefaultTabController(
+                  length: 3,
+                  child: Expanded(
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.r),
+                              color: const Color.fromRGBO(251, 251, 251, 1)
+                          ),
+                          child: TabBar(
+                              tabs: Vocabulary.housingState.map((e) => Tab(text: e,)).toList()
+                          ),
                         ),
-                        child: TabBar(
-                            tabs: Vocabulary.housingState.map((e) => Tab(text: e,)).toList()
-                        ),
-                      ),
-                      Expanded(
-                        child: TabBarView(
-                            children: Vocabulary.housingState.map(
-                                    (e) => NoticesTabPage()
-                            ).toList()
-                        ),
-                      )
-                    ],
-                  ),
-                )
-            )
-          ],
+                        Expanded(
+                          child: TabBarView(
+                              children: Vocabulary.housingState.map(
+                                      (e) => NoticesTabPage()
+                              ).toList()
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+              )
+            ],
+          ),
         ),
       ),
     );
