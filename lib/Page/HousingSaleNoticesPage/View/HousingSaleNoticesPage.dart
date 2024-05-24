25e2c88c4a49dc2161676384c331b141bbdab343
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-05-24 18:22:29 +0900

    CustomSearchBar 추가

--- lib/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart
+++ lib/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:homerun/Style/Palette.dart';
+import 'package:homerun/Widget/CustonSearchBar.dart';
 
 class HousingSaleNoticesPage extends StatelessWidget {
   const HousingSaleNoticesPage({super.key});
@@ -21,6 +21,7 @@ class HousingSaleNoticesPage extends StatelessWidget {
                 ),
               ),
             ),
+            CustomSearchBar()
           ],
         ),
       ),
