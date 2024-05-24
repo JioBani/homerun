67dbf7603d038341c29bdc0ef90214db3874c0b3
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-05-24 16:50:05 +0900

    분양공고 이동 텍스트 버튼 추가

--- lib/Page/Test/View/TestPage.dart
+++ lib/Page/Test/View/TestPage.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:homerun/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart';
 
 class TestPage extends StatelessWidget {
   const TestPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            
+            TextButton(
+                onPressed: (){
+                  Get.to(HousingSaleNoticesPage());
+                },
+                child: Text("분양공고 페이지")
+            )
           ],
         ),
       ),
