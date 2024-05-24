79a32960b4b8fb2fd3e243f909f45fdaf93cc9e2
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-05-24 18:04:19 +0900

    청약홈런 로고 텍스트 추가

--- lib/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart
+++ lib/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:homerun/Style/Palette.dart';
 
 class HousingSaleNoticesPage extends StatelessWidget {
   const HousingSaleNoticesPage({super.key});
@@ -10,7 +12,15 @@ class HousingSaleNoticesPage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            
+            Center(
+              child: Text("청약홈런",
+                style: TextStyle(
+                    fontSize: 30.sp,
+                    fontWeight: FontWeight.w600,
+                    color: Palette.defaultBlue
+                ),
+              ),
+            ),
           ],
         ),
       ),
