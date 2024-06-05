cb30acad87707acdc8bbe56a97cd7d73bb8efe1a
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-05 20:14:07 +0900

    add : 제휴광고 및 광고문의 아이콘 추가

--- lib/Style/Images.dart
+++ lib/Style/Images.dart
@@ -1,6 +1,8 @@
 class Images{
   static const String homeTemp = "assets/images/home_temp.jpg";
   static const String backIcon = "assets/images/back.png";
+  static const String adInquiry = "assets/images/ad_inquiry.png";
+  static const String partnershipAd = "assets/images/partnership_ad.png";
 }
 
 class HomeImages{
