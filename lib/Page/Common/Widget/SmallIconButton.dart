a0264e57b8884a4cbf5d37e1d2bfaf794977e72e
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-06 02:54:19 +0900

    add : SmallIconButton 생성

---
+++ lib/Page/Common/Widget/SmallIconButton.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SmallIconButton extends StatelessWidget {
+  const SmallIconButton({super.key, required this.text, required this.iconPath, required this.onTap});
+  final String iconPath;
+  final String text;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: ()=>onTap(),
+      child: Column(
+        children: [
+          SizedBox(
+            width: 15.sp,
+            height: 15.sp,
+            child: Image.asset(
+              iconPath,
+            ),
+          ),
+          SizedBox(height: 3.w,),
+          Text(
+            text,
+            style: TextStyle(
+                fontSize: 7.sp
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
