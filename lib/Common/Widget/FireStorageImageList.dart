9d943ed6aba4b798977c432d9a0fb3b19671435c
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-06 15:50:01 +0900

    add : Firebase Storage의 특정 경로에 저장된 이미지들을 [FireStorageImage]들의 Colum으로 출력해주는 위젯.

---
+++ lib/Common/Widget/FireStorageImageList.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:homerun/Common/FirebaseStorageImage.dart';
+
+/// **FireStorageImageList**
+///
+/// Firebase Storage의 특정 경로에 저장된 이미지들을 [FireStorageImage]들의 Colum으로 출력해주는 위젯.
+///
+/// [path] : 이 경로는 Firebase Storage에서 이미지가 위치한 폴더를 지정합니다.
+class FireStorageImageList extends StatefulWidget {
+  const FireStorageImageList({super.key, required this.path});
+  final String path;
+
+  @override
+  State<FireStorageImageList> createState() => _FireStorageImageListState();
+}
+
+class _FireStorageImageListState extends State<FireStorageImageList> {
+  ListResult? listResult;
+
+  @override
+  void initState() {
+    _loadImagesPath();
+    super.initState();
+  }
+
+  /// 지정된 Firebase Storage 경로아래의 이미지 경로들을 가져옵니다.
+  Future<void> _loadImagesPath() async {
+    final storageRef = FirebaseStorage.instance.ref().child(widget.path);
+    listResult = await storageRef.listAll();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: listResult?.items.map((e) => FireStorageImage(path: e.fullPath)).toList() ?? [],
+    );
+  }
+}
