8e0c662c0ca6ea035efe05a6680c5cb77768813e
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-07 19:24:20 +0900

    add : NaverGeocodeService 추가

---
+++ lib/Service/NaverGeocodeService.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:homerun/Model/NaverMap/NaverMapGeoCode.dart';
+
+class NaverGeocodeService {
+  late final String? _clientId;
+  late final String? _clientSecret;
+
+  NaverGeocodeService._();
+
+  static NaverGeocodeService? _instance;
+  static NaverGeocodeService get instance {
+    _instance ??= NaverGeocodeService._();
+    return _instance!;
+  }
+
+  void init(String clientId, String clientSecret){
+    _clientId = clientId;
+    _clientSecret = clientSecret;
+  }
+
+  Future<NaverMapGeocode> fetchGeocode(String query, {String? coordinate}) async {
+
+    if(_clientId == null){
+      throw Exception('client id is not initialized');
+    }
+
+    if(_clientSecret == null){
+      throw Exception('client id is not initialized');
+    }
+
+    final uri = Uri.https('naveropenapi.apigw.ntruss.com', '/map-geocode/v2/geocode', {
+      'query': query,
+      if (coordinate != null) 'coordinate': coordinate,
+    });
+
+    final response = await http.get(
+      uri,
+      headers: {
+        'X-NCP-APIGW-API-KEY-ID': _clientId!,
+        'X-NCP-APIGW-API-KEY': _clientSecret!,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return NaverMapGeocode.fromMap(json.decode(response.body));
+    } else {
+      throw Exception('Failed to fetch geocode data');
+    }
+  }
+}
