dbc68d456782a15305f318fea36a737e163fdcfd
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-08 18:39:24 +0900

    style : 필요없는 import 제거

--- lib/Page/NoticesPage/View/LocationMap.dart
+++ lib/Page/NoticesPage/View/LocationMap.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
