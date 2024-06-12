d1505e8c62df965b01b532af47b21f8861774a75
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-12 14:47:10 +0900

    feat : 네트워크 예외처리

--- lib/Page/NoticesPage/Controller/SiteReviewWidgetController.dart
+++ lib/Page/NoticesPage/Controller/SiteReviewWidgetController.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:homerun/Common/LoadingState.dart';
+import 'package:homerun/Common/StaticLogger.dart';
 import 'package:homerun/Page/NoticesPage/Model/SiteReview.dart';
 
 class SiteReviewWidgetController extends GetxController{
@@ -14,7 +15,14 @@ class SiteReviewWidgetController extends GetxController{
   Future<void> loadReviews() async {
     reviews = [];
     loadingState.value = LoadingState.loading;
-    QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('site_review').doc('test').collection('review').get();
-    reviews = querySnapshot.docs.map((doc) => SiteReview.fromMap(doc.data() as Map<String, dynamic>)).toList();
+    try{
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('site_review').doc('test').collection('review').get();
+      reviews = querySnapshot.docs.map((doc) => SiteReview.fromMap(doc.data() as Map<String, dynamic>)).toList();
+      loadingState.value = LoadingState.success;
+    }catch(e ,s){
+      StaticLogger.logger.e("[SiteReviewWidgetController.loadReviews()] $e\n$s");
+      reviews = [];
+      loadingState.value = LoadingState.fail;
+    }
   }
 }
