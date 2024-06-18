4adbed42f77f6c3acd94cb15180b5cdc03b350a0
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-18 20:29:06 +0900

    add : 댓글 보기 위젯 생성

---
+++ lib/Page/NoticesPage/View/CommentViewWIdget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommentViewWidget extends StatelessWidget {
+  const CommentViewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
