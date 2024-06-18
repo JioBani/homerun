3aa7603644c3304342d5943f1ad7874e9d1ce6bd
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-18 17:07:38 +0900

    feat : Gender와 SocialProvider 수정에 따라서 UserDto 수정

--- lib/Service/Auth/UserDto.dart
+++ lib/Service/Auth/UserDto.dart
@@ -1,36 +1,16 @@
-enum Gender {
-  none,
-  male,
-  female,
-}
-
-extension GenderExtension on Gender {
-  String toShortString() {
-    return this.toString().split('.').last;
-  }
-
-  static Gender fromString(String genderString) {
-    switch (genderString) {
-      case 'male':
-        return Gender.male;
-      case 'female':
-        return Gender.female;
-      default:
-        return Gender.none;
-    }
-  }
-}
+import 'package:homerun/Common/enum/Gender.dart';
+import 'package:homerun/Service/Auth/SocialProvider.dart';
 
 class UserDto {
-  String uid;
-  String provider;
-  String displayName;
-  String birth;
-  Gender gender;
+  final String uid;
+  final SocialProvider socialProvider;
+  final String displayName;
+  final String birth;
+  final Gender gender;
 
   UserDto({
+    required this.socialProvider,
     required this.uid,
-    required this.provider,
     required this.displayName,
     required this.birth,
     required this.gender,
@@ -39,7 +19,7 @@ class UserDto {
   factory UserDto.fromMap(Map<String, dynamic> map) {
     return UserDto(
       uid: map['uid'] as String,
-      provider: map['provider'] as String,
+      socialProvider: SocialProviderExtension.fromString(map['socialProvider']),
       displayName: map['displayName'] as String,
       birth: map['birth'] as String,
       gender: GenderExtension.fromString(map['gender'] as String),
@@ -48,17 +28,17 @@ class UserDto {
 
   Map<String, dynamic> toMap() {
     return {
+      'socialProvider': socialProvider.name,
       'uid': uid,
-      'provider': provider,
       'displayName': displayName,
       'birth': birth,
-      'gender': gender.toShortString(),
+      'gender': gender.name,
     };
   }
 
   UserDto.test()
       : uid = 'testUid',
-        provider = 'testProvider',
+        socialProvider = SocialProvider.kakao,
         displayName = 'testNickName',
         birth = '2000-01-01',
         gender = Gender.none;
