747e97cd8d7b52232156c557936adab5c2406fa7
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-19 21:10:30 +0900

    feat : 등록 텍스트 적용

--- lib/Page/NoticesPage/View/CommentInputWidget.dart
+++ lib/Page/NoticesPage/View/CommentInputWidget.dart
@@ -1,17 +1,22 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:homerun/Common/StaticLogger.dart';
+import 'package:homerun/Page/NoticesPage/Service/CommentService.dart';
 
 class CommentInputWidget extends StatelessWidget {
-  CommentInputWidget({super.key});
+  CommentInputWidget({super.key, required this.noticeId});
+  final String noticeId;
 
   final TextEditingController _controller = TextEditingController();
 
-  void _submitComment() {
-    if (_controller.text.isNotEmpty) {
-      print('Submitted comment: ${_controller.text}');
-      _controller.clear();
+  Future<void> _submitComment() async {
+    try{
+      //await CommentService().delete(noticeId,'f2Y7kbN9sV4Aj2mbyeUL');
+      if(_controller.text.isNotEmpty){
+        await CommentService().uploadComment(_controller.text,noticeId);
+      }
+    }catch(e){
+      StaticLogger.logger.e(e);
     }
   }
 
@@ -27,13 +32,12 @@ class CommentInputWidget extends StatelessWidget {
           borderRadius: BorderRadius.circular(20.r),
         ),
         child: TextFormField(
+          controller: _controller,
           decoration: InputDecoration(
               contentPadding: EdgeInsets.only(top: 7.5.sp , bottom: 7.5.sp,left: 7.5.w),
               hintText: hintText,
-              isCollapsed: true,
               isDense :true,
               filled: true,
-              //fillColor: Colors.redAccent,
               fillColor: Color(0xffFBFBFB),
               hintStyle: TextStyle(
                   fontSize: 11.sp,
@@ -50,7 +54,20 @@ class CommentInputWidget extends StatelessWidget {
               enabledBorder: OutlineInputBorder(
                 borderSide: BorderSide(color: Color(0xffA4A4A6) , width: 0.5.sp),
                 borderRadius: BorderRadius.circular(3.r),
-              )
+              ),
+              suffixIcon: InkWell(
+                onTap: _submitComment,
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    '등록',
+                    style: TextStyle(
+                      fontSize: 11.sp,
+                      color: Theme.of(context).colorScheme.secondary
+                    ),
+                  ),
+                ),
+              ),
           ),
           style: TextStyle(
               fontSize: 11.sp
