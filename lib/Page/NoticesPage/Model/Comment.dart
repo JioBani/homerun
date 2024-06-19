c49d09adf4332fb8f0ecd725d7ac406aab8c7006
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-19 18:22:23 +0900

    feat : uid 추가하고 final로 변경

--- lib/Page/NoticesPage/Model/Comment.dart
+++ lib/Page/NoticesPage/Model/Comment.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Comment {
-  String displayName;
-  Timestamp date;
-  int good;
-  int bad;
-  String content;
+  final String displayName;
+  final Timestamp date;
+  final int good;
+  final int bad;
+  final String content;
+  final String uid;
 
   Comment({
     required this.displayName,
@@ -13,6 +14,7 @@ class Comment {
     required this.good,
     required this.bad,
     required this.content,
+    required this.uid
   });
 
   factory Comment.fromMap(Map<String, dynamic> map) {
@@ -22,6 +24,7 @@ class Comment {
       good: map['good'],
       bad: map['bad'],
       content: map['content'],
+      uid: map['uid']
     );
   }
 
@@ -32,6 +35,16 @@ class Comment {
       'good': good,
       'bad': bad,
       'content': content,
+      'uid' : uid
     };
   }
+
+  Comment.test()
+      : displayName = 'Test User',
+        date = Timestamp.now(),
+        good = 0,
+        bad = 0,
+        content = '청년특공 어떻게 될까요? 부모님과 함께 살고 있는데도 가능할까요? 부린이라 모르는게 너무 많아요....ㅠ_ㅠ',
+        uid = 'test';
+
 }
