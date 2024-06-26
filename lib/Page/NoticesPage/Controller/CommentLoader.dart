3f4e59a0bd758a4a552381021841c80ba4f35a63
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-27 00:55:33 +0900

    feat : getComment 함수 추가 및 적용

--- lib/Page/NoticesPage/Controller/CommentLoader.dart
+++ lib/Page/NoticesPage/Controller/CommentLoader.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:homerun/Common/LoadingState.dart';
 import 'package:homerun/Common/StaticLogger.dart';
 import 'package:homerun/Page/Common/FirebaseResponse.dart';
 import 'package:homerun/Page/NoticesPage/Model/Comment.dart';
+import 'package:homerun/Page/NoticesPage/Model/CommentDto.dart';
 
 import 'CommentViewWidgetController.dart';
 
@@ -24,16 +26,60 @@ class CommentLoader{
         action: () => query.get()
     );
 
-    if(response.isSuccess){
-      comments = (response.response!.docs.map((e) =>  Comment.fromMap(e.data() as Map<String , dynamic> , e.id))).toList();
+    if(response.isSuccess){      
+      List<CommentDto> commentDtoList = (response.response!.docs.map((e) =>
+          CommentDto.fromMap(e.data() as Map<String , dynamic>))
+      ).toList();
+
+      String? uid = FirebaseAuth.instance.currentUser?.uid;
+
+      comments = await Future.wait(response.response!.docs.map((e) => getComment(e , uid)));
+
       StaticLogger.logger.i('[CommentViewWidgetController.loadComments()] 댓글 가져오기 성공 :${response.exception} ${response.response!.docs.length}');
       loadingState = LoadingState.success;
       controller.update();
+
     }
     else{
       StaticLogger.logger.e('[CommentViewWidgetController.loadComments()] 댓글 가져오기 실패 :${response.exception}');
       loadingState = LoadingState.fail;
       controller.update();
+    }
+  }
+
+  Future<Comment> getComment(DocumentSnapshot doc, String? uid) async {
+    try{
+      int likeState;
+
+      if(uid != null){
+        DocumentSnapshot likeDoc = await doc.reference.collection('likes').doc(uid).get();
+
+        if(likeDoc.exists){
+          likeState = likeDoc['value'] as int;
+        }
+        else{
+          likeState = 0;
+        }
+      }
+      else{
+        likeState = 0;
+      }
+
+      CommentDto commentDto = CommentDto.fromMap(doc.data() as Map<String, dynamic>);
+
+      return Comment(
+          commentId: doc.id,
+          commentDto: commentDto,
+          likeState: likeState
+      );
+    }
+    catch(e){
+      StaticLogger.logger.e('[CommentLoader.getComment()] ${doc.id} : $e');
+      return Comment(
+          commentId: doc.id,
+          commentDto: CommentDto.fromMap(doc.data() as Map<String, dynamic>),
+          likeState: 0
+      );
     }
   }
 
