867829741160c6c22b950cc8e90dc80e40054884
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-27 00:55:06 +0900

    feat : commentDto, likestate로 변경

--- lib/Page/NoticesPage/Model/Comment.dart
+++ lib/Page/NoticesPage/Model/Comment.dart
@@ -1,53 +1,39 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:homerun/Page/NoticesPage/Model/CommentDto.dart';
 
 class Comment {
   final String commentId;
-  final String displayName;
-  final Timestamp date;
-  final int like;
-  final int dislike;
-  final String content;
-  final String uid;
+  final CommentDto commentDto;
+  final int likeState;
 
   Comment({
     required this.commentId,
-    required this.displayName,
-    required this.date,
-    required this.like,
-    required this.dislike,
-    required this.content,
-    required this.uid
+    required this.commentDto,
+    required this.likeState
   });
 
-  factory Comment.fromMap(Map<String, dynamic> map , String commentId) {
+  factory Comment.fromMap(
+      String commentId ,
+      Map<String, dynamic> map,
+      int likeState
+    ) {
     return Comment(
         commentId: commentId,
-        displayName: map['displayName'],
-        date: map['date'],
-        like: map['like'],
-        dislike: map['dislike'],
-        content: map['content'],
-        uid: map['uid']
+        commentDto: CommentDto.fromMap(map),
+        likeState: likeState
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      'displayName': displayName,
-      'date': date,
-      'like': like,
-      'dislike': dislike,
-      'content': content,
-      'uid' : uid
+      'commentId': commentId,
+      'commentDto': commentDto.toMap(),
+      'likeState': likeState
     };
   }
 
   Comment.test()
       : commentId = 'test',
-        displayName = 'Test User',
-        date = Timestamp.now(),
-        like = 0,
-        dislike = 0,
-        content = '청년특공 어떻게 될까요? 부모님과 함께 살고 있는데도 가능할까요? 부린이라 모르는게 너무 많아요....ㅠ_ㅠ',
-        uid = 'test';
+        commentDto = CommentDto.test(),
+        likeState = 0;
+
 }
