de4fb5047229253d009f3fe1bb39135e1402f6fe
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-28 16:00:31 +0900

    add : 현장리뷰 리스트 페이지 생성

---
+++ lib/Page/NoticesPage/View/SiteReview/SiteReviewListPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SiteReviewListPage extends StatelessWidget {
+  const SiteReviewListPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text("gd"),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
