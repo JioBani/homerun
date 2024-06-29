c38879d85578295dae05754b23306238f2e11dda
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-29 16:36:56 +0900

    feat : id 추가 및 upload용 toMap 추가

--- lib/Page/NoticesPage/Model/SiteReview.dart
+++ lib/Page/NoticesPage/Model/SiteReview.dart
@@ -1,22 +1,39 @@
 class SiteReview {
+  String id;
   String noticeId;
   String title;
   String content;
   String writer;
   int view;
   String imagesRefPath;
+  String thumbnailRefPath;
 
 
   SiteReview({
+    required this.id,
     required this.noticeId,
     required this.title,
     required this.content,
     required this.writer,
     required this.view,
     required this.imagesRefPath,
+    required this.thumbnailRefPath
   });
 
   Map<String, dynamic> toMap() {
+    return {
+      'id' : id,
+      'noticeId' : noticeId,
+      'title': title,
+      'content': content,
+      'writer': writer,
+      'view': view,
+      'imagesRefPath': imagesRefPath,
+      'thumbnailRefPath': thumbnailRefPath,
+    };
+  }
+
+  Map<String, dynamic> toUploadMap() {
     return {
       'noticeId' : noticeId,
       'title': title,
@@ -24,28 +41,33 @@ class SiteReview {
       'writer': writer,
       'view': view,
       'imagesRefPath': imagesRefPath,
+      'thumbnailRefPath': thumbnailRefPath,
     };
   }
 
-  factory SiteReview.fromMap(Map<String, dynamic> map) {
+  factory SiteReview.fromMap(Map<String, dynamic> map , String id) {
     return SiteReview(
+      id : id,
       noticeId: map['noticeId'],
       title: map['title'],
       content: map['content'],
       writer: map['writer'],
       view: map['view'],
       imagesRefPath: map['imagesRefPath'],
+      thumbnailRefPath: map['thumbnailRefPath'],
     );
   }
 
   factory SiteReview.test() {
     return SiteReview(
+      id : "test",
       noticeId: '1',
       title: "테스트1",
       content: '테스트1',
       writer: 'writer',
       view: 0,
       imagesRefPath: 'site_review/test/1',
+      thumbnailRefPath: 'site_review/test/1',
     );
   }
 }
