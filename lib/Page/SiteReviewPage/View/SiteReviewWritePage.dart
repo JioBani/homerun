02d28843abc06d984175a8ac851b3783e823d6a5
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-29 17:22:35 +0900

    feat : 제목, 본문 입력 창, 버튼 추가

--- lib/Page/SiteReviewPage/View/SiteReviewWritePage.dart
+++ lib/Page/SiteReviewPage/View/SiteReviewWritePage.dart
@@ -1,10 +1,195 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:homerun/Style/Fonts.dart';
 
 class SiteReviewWritePage extends StatelessWidget {
   const SiteReviewWritePage({super.key});
 
+  static const Color subTitleColor = Color(0xff767676);
+  static const Color fillColor = Color(0xffFBFBFB);
+  static const Color borderColor = Color(0xffA4A4A6);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar:AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+             "글쓰기",
+              style: TextStyle(
+                  fontSize:  20.sp,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: Fonts.BCCard,
+                  color: Theme.of(context).primaryColor
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+
+            },
+            child: Text(
+              "임시저장",
+              style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w600,
+                color: subTitleColor
+              ),
+            ),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40.w , vertical: 15.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 2.w),
+                child: Text(
+                  "제목",
+                  style: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
+                      color: subTitleColor
+                  ),
+                ),
+              ),
+              SizedBox(height: 9.w,),
+              CustomTextFormField(
+                hintText: "제목을 입력해주세요",
+                maxLines: 1,
+              ),
+              SizedBox(height: 26.w,),
+              Padding(
+                padding: EdgeInsets.only(left: 2.w),
+                child: Text(
+                  "본문",
+                  style: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
+                      color: subTitleColor
+                  ),
+                ),
+              ),
+              SizedBox(height: 9.w,),
+              Expanded(
+                child: CustomTextFormField(
+                  hintText: "리뷰 내용을 작성해주세요.",
+                  maxLines: 30,
+                ),
+              ),
+              SizedBox(height: 14.w,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 135.w,
+                    height: 30.w,
+                    decoration: BoxDecoration(
+                      color: fillColor,
+                      borderRadius: BorderRadius.circular(5.r),
+                      border: Border.all(color: borderColor)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "파일업로드",
+                        style: TextStyle(
+                          color: subTitleColor,
+                          fontSize: 15.sp
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 135.w,
+                    height: 30.w,
+                    decoration: BoxDecoration(
+                        color: fillColor,
+                        borderRadius: BorderRadius.circular(5.r),
+                        border: Border.all(color: borderColor)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "파일업로드",
+                        style: TextStyle(
+                            color: subTitleColor,
+                            fontSize: 15.sp
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 14.w,),
+              Container(
+                width: double.infinity,
+                height: 30.w,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(5.r),
+                ),
+                child: Center(
+                  child: Text(
+                    "작성완료",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.sp,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({super.key, required this.hintText, required this.maxLines});
+
+  static const Color subTitleColor = Color(0xff767676);
+  static const Color inputFillColor = Color(0xffFBFBFB);
+  final String hintText;
+  final int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      //controller: textEditingController,
+      maxLines: maxLines,
+      cursorColor: const Color(0xFF35C5F0),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: inputFillColor,
+        hintText: hintText,
+        hintStyle: TextStyle(color: subTitleColor , fontSize: 15.sp),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6.r),
+          borderSide: BorderSide(color: const Color(0xffA4A4A6),width: 1.sp),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6.r),
+          borderSide: BorderSide(color: const Color(0xffA4A4A6),width: 1.sp),
+        ),
+        contentPadding: EdgeInsets.symmetric(vertical: 8.w, horizontal: 6.w),
+      ),
+      style: TextStyle(
+          fontSize: 11.sp
+      ),
+    );
   }
 }
