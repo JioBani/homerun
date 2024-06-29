5e05676ed8daa2bbb8d11ff11355fbc937b92495
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-29 15:53:24 +0900

    feat : 리뷰 썸네일 이미지 적용

--- lib/Page/NoticesPage/View/SiteReview/SiteReviewListItemWidget.dart
+++ lib/Page/NoticesPage/View/SiteReview/SiteReviewListItemWidget.dart
@@ -1,25 +1,48 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:homerun/Common/FirebaseStorageImage.dart';
 import 'package:homerun/Page/NoticesPage/Model/SiteReview.dart';
 import 'package:homerun/Style/Images.dart';
 import 'package:homerun/Style/TestImages.dart';
 
-class SiteReviewListItemWidget extends StatelessWidget {
+class SiteReviewListItemWidget extends StatefulWidget {
   const SiteReviewListItemWidget({super.key, required this.siteReview});
   final SiteReview siteReview;
+
+  @override
+  State<SiteReviewListItemWidget> createState() => _SiteReviewListItemWidgetState();
+}
+
+class _SiteReviewListItemWidgetState extends State<SiteReviewListItemWidget> {
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(5.r),
-          child: Image.asset(
-            TestImages.irelia_6,
-            fit: BoxFit.fitWidth,
-            width: 145.w,
-            height: 180.w,
+        SizedBox(
+          width: 145.w,
+          height: 180.w,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(5.r),
+              child: Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.r),
+                      color: Colors.black12
+                    ),
+                  ),
+                  FireStorageImage(
+                    path: widget.siteReview.thumbnailRefPath,
+                    fit: BoxFit.fitHeight,
+                    width: 145.w,
+                    height: 180.w,
+                  ),
+                ],
+              )
           ),
         ),
         SizedBox(height: 11.w,),
@@ -40,7 +63,7 @@ class SiteReviewListItemWidget extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  siteReview.writer,
+                  widget.siteReview.writer,
                   style: TextStyle(
                       fontWeight: FontWeight.w600,
                       fontSize: 11.sp
@@ -53,7 +76,7 @@ class SiteReviewListItemWidget extends StatelessWidget {
                     ),
                     SizedBox(width: 3.w,),
                     Text(
-                      "조회수 ${siteReview.view}",
+                      "조회수 ${widget.siteReview.view}",
                       style: TextStyle(
                           fontSize: 8.sp,
                           color: Color(0xff767676)
