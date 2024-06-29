95335fbcc2ad7f3ffa606f654ec74654130da107
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-06-29 16:40:30 +0900

    add : 현장리뷰 작성 페이지 생성

---
+++ lib/Page/SiteReviewPage/View/SiteReviewWritePage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SiteReviewWritePage extends StatelessWidget {
+  const SiteReviewWritePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
