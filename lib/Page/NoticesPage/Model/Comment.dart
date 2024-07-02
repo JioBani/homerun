ed2c2be681921b56233ae5ff802e458b70b641a6
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-02 15:33:25 +0900

    feat : snapshot 필드 추가

--- lib/Page/NoticesPage/Model/Comment.dart
+++ lib/Page/NoticesPage/Model/Comment.dart
@@ -1,25 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:homerun/Page/NoticesPage/Model/CommentDto.dart';
 
 class Comment {
   final String commentId;
   final CommentDto commentDto;
   final int likeState;
+  final DocumentSnapshot? snapshot;
 
   Comment({
     required this.commentId,
     required this.commentDto,
-    required this.likeState
+    required this.likeState,
+    required this.snapshot
   });
 
   factory Comment.fromMap(
       String commentId ,
       Map<String, dynamic> map,
-      int likeState
+      int likeState,
+      DocumentSnapshot snapshot,
     ) {
     return Comment(
         commentId: commentId,
         commentDto: CommentDto.fromMap(map),
-        likeState: likeState
+        likeState: likeState,
+        snapshot : snapshot
     );
   }
 
@@ -27,13 +32,15 @@ class Comment {
     return {
       'commentId': commentId,
       'commentDto': commentDto.toMap(),
-      'likeState': likeState
+      'likeState': likeState,
+      'snapshot' : snapshot.toString()
     };
   }
 
   Comment.test()
       : commentId = 'test',
         commentDto = CommentDto.test(),
-        likeState = 0;
+        likeState = 0,
+        snapshot = null;
 
 }
