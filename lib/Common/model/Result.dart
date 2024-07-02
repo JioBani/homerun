7d0d5da1f682aa79f5b047c561f603c75cd5ef00
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-02 11:08:04 +0900

    feat : 핸들러 추가

--- lib/Common/model/Result.dart
+++ lib/Common/model/Result.dart
@@ -1,3 +1,5 @@
+import 'package:homerun/Common/StaticLogger.dart';
+
 class Result<T>{
   bool isSuccess;
   T? result;
@@ -12,5 +14,30 @@ class Result<T>{
 
   factory Result.fromFailure(Object? exception, StackTrace? stackTrace){
     return Result(isSuccess: false,exception: exception,stackTrace:stackTrace);
+  }
+
+  static Future<Result<T>> handle<T>({
+    required T Function() action,
+  }) async {
+    try {
+      return Result<T>.fromSuccess(result: action());
+
+    } catch (e , s) {
+      StaticLogger.logger.e('[ApiResponse.handleExceptions()] $e\n$s');
+      return Result<T>.fromFailure(e, s);
+    }
+  }
+
+  static Future<Result<T>> handleFuture<T>({
+    required Future<T> Function() action,
+    Duration timeout = const Duration(seconds: 5), //TODO 적절한 시간으로 수정
+  }) async {
+    try {
+      return Result<T>.fromSuccess(result: await action().timeout(timeout));
+
+    } catch (e , s) {
+      StaticLogger.logger.e('[ApiResponse.handleExceptions()] $e\n$s');
+      return Result<T>.fromFailure(e, s);
+    }
   }
 }
