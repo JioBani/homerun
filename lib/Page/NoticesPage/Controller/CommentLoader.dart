004e9847a7a7eff9bb101eddbdef9614a39b5c60
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-02 15:33:54 +0900

    feat : commentService적용

--- lib/Page/NoticesPage/Controller/CommentLoader.dart
+++ lib/Page/NoticesPage/Controller/CommentLoader.dart
@@ -6,6 +6,7 @@ import 'package:homerun/Common/model/Result.dart';
 import 'package:homerun/Page/Common/FirebaseResponse.dart';
 import 'package:homerun/Page/NoticesPage/Model/Comment.dart';
 import 'package:homerun/Page/NoticesPage/Model/CommentDto.dart';
+import 'package:homerun/Page/NoticesPage/Service/CommentService.dart';
 
 import 'CommentViewWidgetController.dart';
 
@@ -15,71 +16,14 @@ class CommentLoader{
   LoadingState loadingState = LoadingState.before;
   Query query;
   List<DocumentSnapshot> commentSnapshotList = [];
+  CommentService commentService = CommentService.instance;
+  String noticeId;
 
   CommentLoader({
     required this.controller,
-    required this.query
+    required this.query,
+    required this.noticeId,
   });
-
-  Future<void> load() async {
-    loadingState = LoadingState.loading;
-
-    FirebaseResponse<QuerySnapshot> response = await FirebaseResponse.handleRequest<QuerySnapshot>(
-        action: () => query.get()
-    );
-
-    if(response.isSuccess){
-      String? uid = FirebaseAuth.instance.currentUser?.uid;
-
-      comments = await Future.wait(response.response!.docs.map((e) => _getComment(e , uid)));
-
-      StaticLogger.logger.i('[CommentViewWidgetController.loadComments()] 댓글 가져오기 성공 :${response.exception} ${response.response!.docs.length}');
-      loadingState = LoadingState.success;
-      controller.update();
-
-    }
-    else{
-      StaticLogger.logger.e('[CommentViewWidgetController.loadComments()] 댓글 가져오기 실패 :${response.exception}');
-      loadingState = LoadingState.fail;
-      controller.update();
-    }
-  }
-
-  Future<Comment> _getComment(DocumentSnapshot doc, String? uid) async {
-    try{
-      int likeState;
-
-      if(uid != null){
-        DocumentSnapshot likeDoc = await doc.reference.collection('likes').doc(uid).get();
-
-        if(likeDoc.exists){
-          likeState = likeDoc['value'] as int;
-        }
-        else{
-          likeState = 0;
-        }
-      }
-      else{
-        likeState = 0;
-      }
-
-      CommentDto commentDto = CommentDto.fromMap(doc.data() as Map<String, dynamic>);
-
-      return Comment(
-          commentId: doc.id,
-          commentDto: commentDto,
-          likeState: likeState
-      );
-    }
-    catch(e){
-      StaticLogger.logger.e('[CommentLoader.getComment()] ${doc.id} : $e');
-      return Comment(
-          commentId: doc.id,
-          commentDto: CommentDto.fromMap(doc.data() as Map<String, dynamic>),
-          likeState: 0
-      );
-    }
-  }
 
   Future<void> getNextComments(int index , {bool reset = false}) async{
 
@@ -88,41 +32,40 @@ class CommentLoader{
       comments = [];
     }
 
-    FirebaseResponse<QuerySnapshot> response;
+    Result<List<Comment>> result;
 
-    if(commentSnapshotList.isEmpty){
-      response = await FirebaseResponse.handleRequest<QuerySnapshot>(
-          action: () => query.limit(index).get()
+    if(comments.isEmpty){
+      result = await commentService.getComments(
+        noticeId: noticeId,
+        orderBy: SortOrder.latest,
+        type:  CommentType.free,
+        index: index,
       );
     }
     else{
-      response = await FirebaseResponse.handleRequest<QuerySnapshot>(
-          action: () => query.startAfterDocument(commentSnapshotList.last).limit(index).get()
+      result = await commentService.getComments(
+        noticeId: noticeId,
+        orderBy: SortOrder.latest,
+        type:  CommentType.free,
+        index: index,
+        startAfter: comments.last.snapshot
       );
     }
 
-    if(!response.isSuccess){
+    if(!result.isSuccess){
       loadingState = LoadingState.fail;
       controller.update();
       return;
     }
 
-    if(response.response!.docs.isEmpty){
+    if(result.content!.isEmpty){
       loadingState = LoadingState.noMoreData;
       controller.update();
       return;
     }
 
-    String? uid = FirebaseAuth.instance.currentUser?.uid;
-
     try{
-      List<Comment> newComments = await Future.wait(
-          response.response!.docs.map((e) => _getComment(e , uid))
-      );
-
-      comments.addAll(newComments);
-      commentSnapshotList.addAll(response.response!.docs);
-
+      comments.addAll(result.content!);
       loadingState = LoadingState.success;
       controller.update();
       return;
