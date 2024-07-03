b4c8275b724a2781ebe83fd21caa0e18c6da53de
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-03 17:42:11 +0900

    feat : 유저 불러오기 추가 및 shimmer 적용

--- lib/Page/NoticesPage/View/SiteReview/SiteReviewListItemWidget.dart
+++ lib/Page/NoticesPage/View/SiteReview/SiteReviewListItemWidget.dart
@@ -1,10 +1,14 @@
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:homerun/Common/FirebaseStorageImage.dart';
+import 'package:homerun/Common/LoadingState.dart';
+import 'package:homerun/Common/model/Result.dart';
 import 'package:homerun/Page/NoticesPage/Model/SiteReview.dart';
+import 'package:homerun/Service/Auth/UserDto.dart';
+import 'package:homerun/Service/FirebaseFirestoreService.dart';
 import 'package:homerun/Style/Images.dart';
 import 'package:homerun/Style/TestImages.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SiteReviewListItemWidget extends StatefulWidget {
   const SiteReviewListItemWidget({super.key, required this.siteReview});
@@ -15,6 +19,29 @@ class SiteReviewListItemWidget extends StatefulWidget {
 }
 
 class _SiteReviewListItemWidgetState extends State<SiteReviewListItemWidget> {
+
+  UserDto? userDto;
+  LoadingState loadingState = LoadingState.before;
+
+  Future<void> getUser()async {
+    loadingState = LoadingState.loading;
+    setState(() {});
+    Result<UserDto> result = await FirebaseFirestoreService.instance.getUser(widget.siteReview.writer);
+    if(result.isSuccess){
+      userDto = result.content;
+      loadingState = LoadingState.success;
+    }
+    else{
+      loadingState = LoadingState.fail;
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getUser();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,48 +74,97 @@ class _SiteReviewListItemWidgetState extends State<SiteReviewListItemWidget> {
           ),
         ),
         SizedBox(height: 11.w,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(30.w),
-              child: Image.asset(
-                TestImages.ashe_43,
-                width: 30.w,
-                height: 30.w,
-              ),
-            ),
-            SizedBox(width: 7.w,),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.siteReview.writer,
-                  style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 11.sp
-                  ),
-                ),
-                Row(
+        Builder(
+            builder: (context){
+              if(loadingState == LoadingState.success){
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Image.asset(
-                        NoticePageImages.star
-                    ),
-                    SizedBox(width: 3.w,),
-                    Text(
-                      "조회수 ${widget.siteReview.view}",
-                      style: TextStyle(
-                          fontSize: 8.sp,
-                          color: Color(0xff767676)
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(30.w),
+                      child: Image.asset(
+                        TestImages.ashe_43,
+                        width: 30.w,
+                        height: 30.w,
                       ),
+                    ),
+                    SizedBox(width: 7.w,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          userDto?.displayName ?? '알 수 없음',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 11.sp
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Image.asset(
+                                NoticePageImages.star
+                            ),
+                            SizedBox(width: 3.w,),
+                            Text(
+                              "조회수 ${widget.siteReview.view}",
+                              style: TextStyle(
+                                  fontSize: 8.sp,
+                                  color: Color(0xff767676)
+                              ),
+                            )
+                          ],
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
-            )
-          ],
-        )
+                );
+              }
+              else{
+                return Shimmer.fromColors(
+                  baseColor: Colors.grey.shade300,
+                  highlightColor: Colors.grey.shade100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30.w),
+                          color: Colors.grey,
+                        ),
+                        width: 30.w,
+                        height: 30.w,
+                      ),
+                      SizedBox(width: 7.w,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(1.r),
+                              color: Colors.grey,
+                            ),
+                            width: 50.w,
+                            height: 11.sp,
+                          ),
+                          SizedBox(height: 3.w,),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(1.r),
+                              color: Colors.grey,
+                            ),
+                            width: 70.w,
+                            height: 8.sp,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }
+            }
+        ),
       ],
     );
   }
