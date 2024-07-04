548c9b6fd5beed4060247f2e8a7733dc70037d9b
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-04 15:52:07 +0900

    feat : 이미지 사이즈 계산 추가 및 upload에서 예외 추가

--- lib/Page/SiteReviewPage/Controller/SiteReviewWritePageController.dart
+++ lib/Page/SiteReviewPage/Controller/SiteReviewWritePageController.dart
@@ -1,29 +1,46 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
 import 'package:homerun/Common/StaticLogger.dart';
+import 'package:homerun/Common/model/Result.dart';
 import 'package:homerun/Page/SiteReviewPage/Model/SiteReviewWriteDto.dart';
 import 'package:homerun/Page/SiteReviewPage/Service/SiteReviewService.dart';
 import 'package:image_picker/image_picker.dart';
 
+class OutOfImageSizeException implements Exception{
+
+}
+
 class SiteReviewWritePageController extends GetxController{
   final ImagePicker picker = ImagePicker();
   final String noticeId;
+  final double maxSizeMb = 10;
   RxList<XFile> images = RxList([]);
   Rx<XFile?> thumbnailFile = Rx(null);
+  double imageSize = 0;
 
   SiteReviewWritePageController({required this.noticeId});
 
-  Future<void> addImage() async {
-    final List<XFile> pickedFile = await picker.pickMultiImage();
-    images.addAll(pickedFile);
-    update();
+  Future<Result<void>> addImage() async {
+    return await Result.handleFuture(
+        action: () async {
+          final List<XFile> pickedFile = await picker.pickMultiImage();
+
+          images.addAll(pickedFile);
+          calculateTotalImageSize();
+          update();
+        }
+    );
+
   }
 
-  void removeImage(int index){
+  Future<void> removeImage(int index) async {
     if(index < images.length){
       if(images[index] == thumbnailFile.value){
         thumbnailFile.value = null;
       }
       images.removeAt(index);
+      await calculateTotalImageSize();
       update();
     }
   }
@@ -35,7 +52,12 @@ class SiteReviewWritePageController extends GetxController{
     }
   }
 
-  Future<void> upload(String title, String content){
+  Future<Result<void>> upload(String title, String content) async{
+
+    if(imageSize > maxSizeMb){
+      return Result<void>.fromFailure(OutOfImageSizeException(), StackTrace.current);
+    }
+
     return SiteReviewService.instance.write(
         SiteReviewWriteDto(
             noticeId: noticeId,
@@ -44,17 +66,26 @@ class SiteReviewWritePageController extends GetxController{
             thumbnail: thumbnailFile.value?.name ?? images.first.name
         ),
         images,
-        (state,message,exception){
-          if(state == UploadState.progress){
+            (state,message,exception) {
+          if (state == UploadState.progress) {
             StaticLogger.logger.i(message);
           }
-          else if(state == UploadState.success){
+          else if (state == UploadState.success) {
             StaticLogger.logger.i("업로드 성공");
           }
-          else{
+          else {
             StaticLogger.logger.e("$message : $exception");
           }
         }
     );
+  }
+
+  Future<void> calculateTotalImageSize() async {
+    double totalSize = 0;
+    for (XFile image in images) {
+      final file = File(image.path);
+      totalSize += await file.length();
+    }
+    imageSize = (totalSize / (1024 * 1024));
   }
 }
