fec41f52a8c4d19ff48970fe281fb85b6af77829
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-04 15:56:55 +0900

    add  : 리뷰 보기 페이지 생성

---
+++ lib/Page/SiteReviewPage/View/SiteReviewPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:homerun/Page/NoticesPage/Model/SiteReview.dart';
+
+class SiteReviewPage extends StatelessWidget {
+  const SiteReviewPage({super.key, required this.siteReview});
+  final SiteReview siteReview;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
