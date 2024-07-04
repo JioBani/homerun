b355ef58094d3a9ba45c41c367ecefcc31b1e364
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-04 19:03:54 +0900

    feat : ColorMode제작 및 밝은 테마 글씨색 추가

--- lib/Style/Palette.dart
+++ lib/Style/Palette.dart
@@ -10,6 +10,7 @@ class Palette{
   static final FontPalette font = FontPalette();
   static const Color defaultSkyBlue =  Color(0xff35C5F0);
   static const primary = Color(0xff35C5F0);
+  static ColorMode brightMode = BrightMode();
 
 }
 
@@ -21,4 +22,27 @@ class AssessmentPagePalette{
 class FontPalette{
   final Color grey = const Color.fromRGBO(164, 164, 166, 1);
   final Color blue = const Color.fromRGBO(46, 60, 107, 1);
+}
+
+abstract class ColorMode{
+  Color get darkText;
+  set darkText(Color value);
+
+  Color get mediumText;
+  set mediumText(Color value);
+
+  Color get lightText;
+  set lightText(Color value);
+}
+
+class BrightMode implements ColorMode{
+  @override
+  Color darkText = const Color(0xff555555);
+
+  @override
+  Color mediumText = const Color(0xff767676);
+
+  @override
+  Color lightText = const Color(0xffD9D9D9);
+
 }
