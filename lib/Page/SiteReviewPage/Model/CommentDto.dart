dbd836db56f424b5ec6cc19d68f347ed45bb23c4
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-05 02:54:12 +0900

    add : SiteReview CommentDto 추가

---
+++ lib/Page/SiteReviewPage/Model/CommentDto.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CommentDto {
+  final Timestamp date;
+  final String content;
+  final String uid;
+
+  CommentDto({
+    required this.date,
+    required this.content,
+    required this.uid
+  });
+
+  factory CommentDto.fromMap(Map<String, dynamic> map) {
+    return CommentDto(
+        date: map['date'],
+        content: map['content'],
+        uid: map['uid']
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'date': date,
+      'content': content,
+      'uid' : uid
+    };
+  }
+
+  CommentDto.test() :
+        date = Timestamp.now(),
+        content = '청년특공 어떻게 될까요? 부모님과 함께 살고 있는데도 가능할까요? 부린이라 모르는게 너무 많아요....ㅠ_ㅠ',
+        uid = 'test';
+}
