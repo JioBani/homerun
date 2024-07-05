94b0284d1b058171146e84399eb0166380039a9f
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-05 20:30:13 +0900

    feat : 댓글 보기 위젯 컨트롤러 생성

---
+++ lib/Page/SiteReviewPage/Controller/CommentViewWidgetController.dart
@@ -0,0 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:homerun/Common/Comment/Comment.dart';
+import 'package:homerun/Common/Comment/CommentService.dart';
+import 'package:homerun/Common/Firebase/FirestoreReferences.dart';
+import 'package:homerun/Common/LoadingState.dart';
+import 'package:homerun/Common/StaticLogger.dart';
+import 'package:homerun/Common/model/Result.dart';
+import 'package:homerun/Service/Auth/AuthService.dart';
+
+class CommentViewWidgetController extends GetxController{
+  final String noticeId;
+  final String reviewId;
+  LoadingState loadingState = LoadingState.before;
+  List<Comment> comments = [];
+  final int commentNum = 3;
+  late final CollectionReference commentCollection ;
+
+  static makeTag(String noticeId, String reviewId){
+    return "$noticeId:$reviewId";
+  }
+
+  CommentViewWidgetController({required this.noticeId , required this.reviewId}){
+    commentCollection = FirestoreReferences.getSiteReviewComment(noticeId, reviewId);
+  }
+
+  @override
+  onInit(){
+    super.onInit();
+    getNextComments();
+  }
+
+  Future<void> getNextComments() async {
+    loadingState = LoadingState.loading;
+
+
+    Result<List<Comment>> result = await CommentService.instance.getComments(
+        commentCollection: commentCollection,
+        noticeId: noticeId,
+        reviewId: reviewId,
+        index: commentNum,
+        orderBy: OrderType.date,
+        startAfter: comments.isEmpty ? null : comments.last
+    );
+
+    if(result.isSuccess){
+      comments.addAll(result.content!);
+      StaticLogger.logger.i('댓글 불러오기 성공');
+      loadingState = LoadingState.success;
+    }
+    else{
+      StaticLogger.logger.i('댓글 불러오기 실패');
+      loadingState = LoadingState.fail;
+    }
+
+    update();
+  }
+
+  Future<Result<void>> removeComment(Comment comment) async{
+    Result<void> result = await CommentService.instance.remove(comment);
+
+    if(result.isSuccess){
+      comments.remove(comment);
+      Get.snackbar("알림", "댓글을 삭제하였습니다.");
+    }
+    else{
+      if(result.exception is ApplicationUnauthorizedException){
+        Get.snackbar("오류", "로그인이 필요합니다.");
+      }
+      else{
+        Get.snackbar("오류", "댓글 삭제에 실패하였습니다.");
+      }
+    }
+
+    update();
+
+    return result;
+  }
+}
