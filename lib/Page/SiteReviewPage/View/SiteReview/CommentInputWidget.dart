da3b17a2fb12bd23f0c9927a2ed190ae3708fb83
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-06 00:21:07 +0900

    feat : 제출 버튼 추가 및 색 변경

--- lib/Page/SiteReviewPage/View/SiteReview/CommentInputWidget.dart
+++ lib/Page/SiteReviewPage/View/SiteReview/CommentInputWidget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:homerun/Style/Palette.dart';
 
 class CommentInputWidget extends StatefulWidget {
   const CommentInputWidget({super.key});
@@ -34,12 +35,12 @@ class _CommentInputWidgetState extends State<CommentInputWidget> {
   Widget build(BuildContext context) {
     return AnimatedContainer(
       padding: EdgeInsets.fromLTRB(10.w, 4.w, 10.w, 4.w),
-      duration: Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 300),
       curve: Curves.easeInOut,
       decoration: BoxDecoration(
           color: Colors.white,
           boxShadow: [BoxShadow(
-              offset: Offset(0,0) ,
+              offset: const Offset(0,0) ,
               blurRadius: 4.sp,
               color: Colors.black.withOpacity(0.25)
           )]
@@ -55,38 +56,58 @@ class _CommentInputWidgetState extends State<CommentInputWidget> {
               filled: true,
               fillColor: const Color(0x00FBFBFB),
               hintText: ' 댓글을 입력해주세요.',
-              hintStyle: TextStyle(color: const Color(0xFFD9D9D9) , fontSize: 14.sp),
+              hintStyle: TextStyle(color: Palette.brightMode.lightText , fontSize: 14.sp),
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(3.r),
-                borderSide: BorderSide(color: const Color(0xFFD9D9D9),width: 1.sp),
+                borderSide: BorderSide(color: Palette.brightMode.lightText,width: 1.sp),
               ),
               focusedBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(3.r),
-                borderSide: BorderSide(color: const Color(0xFFD9D9D9),width: 1.sp),
+                borderSide: BorderSide(color: Palette.brightMode.lightText,width: 1.sp),
               ),
               contentPadding: EdgeInsets.symmetric(vertical: 4.w, horizontal: 6.w),
             ),
             style: TextStyle(
-                fontSize: 11.sp
+              fontSize: 14.sp,
+              color: Palette.brightMode.mediumText
             ),
           ),
           Builder(
             builder: (context){
               if(_focusNode.hasFocus){
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
+                return Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 5.w, 0, 3.w),
+                    child: SizedBox(
+                      width: 50.w,
+                      height: 25.w,
+                      child: ElevatedButton(
                         onPressed: (){
-                          _focusNode.unfocus();
+                    
                         },
-                        child: Text("제출")
-                    )
-                  ],
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          shape:  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(3.r),
+                          ),
+                          padding: const EdgeInsets.symmetric(horizontal: 0 , vertical: 0)
+                        ),
+                        child: Text(
+                          "등록",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 12.sp
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 );
               }
               else{
-                return SizedBox(height: 0,);
+                return const SizedBox(height: 0,);
               }
             }
           )
