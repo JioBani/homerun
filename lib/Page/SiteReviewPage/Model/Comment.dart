1998de44e11e7919f21ff4aaf3a46bbd9ae27474
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-05 20:32:11 +0900

    remove : 댓글 모델 제거

--- lib/Page/SiteReviewPage/Model/Comment.dart
+++
@@ -1,8 +0,0 @@
-import 'package:homerun/Page/SiteReviewPage/Model/CommentDto.dart';
-
-class Comment{
-  CommentDto commentDto;
-  String id;
-
-  Comment({required this.id, required this.commentDto});
-}
