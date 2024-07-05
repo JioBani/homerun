ff838c721d6ed9444b340819d355c551e73ee741
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-05 16:22:07 +0900

    add : Comment 모델 추가

---
+++ lib/Page/SiteReviewPage/Model/Comment.dart
@@ -0,0 +1,8 @@
+import 'package:homerun/Page/SiteReviewPage/Model/CommentDto.dart';
+
+class Comment{
+  CommentDto commentDto;
+  String id;
+
+  Comment({required this.id, required this.commentDto});
+}
