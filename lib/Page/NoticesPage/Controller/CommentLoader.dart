d94401e7c0c5cca938171ca14943a9608a8b0904
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-07 21:07:41 +0900

    feat : Comment/CommentService 적용

--- lib/Page/NoticesPage/Controller/CommentLoader.dart
+++ lib/Page/NoticesPage/Controller/CommentLoader.dart
@@ -1,78 +1,63 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:homerun/Common/Comment/Comment.dart';
+import 'package:homerun/Common/Comment/CommentService.dart';
+import 'package:homerun/Common/Firebase/FirestoreReferences.dart';
 import 'package:homerun/Common/LoadingState.dart';
 import 'package:homerun/Common/StaticLogger.dart';
 import 'package:homerun/Common/model/Result.dart';
-import 'package:homerun/Page/Common/FirebaseResponse.dart';
-import 'package:homerun/Page/NoticesPage/Model/Comment.dart';
-import 'package:homerun/Page/NoticesPage/Model/CommentDto.dart';
-import 'package:homerun/Page/NoticesPage/Service/CommentService.dart';
 
 import 'CommentViewWidgetController.dart';
 
 class CommentLoader{
-  CommentViewWidgetController controller;
+  final CommentViewWidgetController controller;
+  final String noticeId;
+  late final CollectionReference collection;
+  final NoticeCommentType commentType;
+  final OrderType orderType;
+
   List<Comment> comments = [];
   LoadingState loadingState = LoadingState.before;
-  List<DocumentSnapshot> commentSnapshotList = [];
-  CommentService commentService = CommentService.instance;
-  String noticeId;
 
   CommentLoader({
     required this.controller,
     required this.noticeId,
-  });
+    required this.commentType,
+    required this.orderType,
+  }){
+    collection = FirestoreReferences.getNoticeComment(noticeId,commentType.name);
+  }
 
-  Future<void> getNextComments(int index , {bool reset = false}) async{
-
+  Future<Result<List<Comment>>> getComments(int index, {bool reset = false}) async {
+    loadingState = LoadingState.loading;
     if(reset){
-      commentSnapshotList = [];
       comments = [];
     }
 
-    Result<List<Comment>> result;
-
-    if(comments.isEmpty){
-      result = await commentService.getComments(
-        noticeId: noticeId,
-        orderBy: SortOrder.latest,
-        type:  CommentType.free,
+    Result<List<Comment>> result = await CommentService.instance.getComments(
+        commentCollection: collection,
+        orderBy: orderType,
         index: index,
-      );
-    }
-    else{
-      result = await commentService.getComments(
-        noticeId: noticeId,
-        orderBy: SortOrder.latest,
-        type:  CommentType.free,
-        index: index,
-        startAfter: comments.last.snapshot
-      );
-    }
+        startAfter: comments.isNotEmpty ? comments.last : null,
+    );
 
     if(!result.isSuccess){
       loadingState = LoadingState.fail;
+      StaticLogger.logger.e(result.exception);
       controller.update();
-      return;
+      return result;
     }
-
-    if(result.content!.isEmpty){
-      loadingState = LoadingState.noMoreData;
-      controller.update();
-      return;
-    }
-
-    try{
+    else{
       comments.addAll(result.content!);
-      loadingState = LoadingState.success;
-      controller.update();
-      return;
-    }catch(e,s){
-      StaticLogger.logger.e('[CommentLoader.getNextComments()] $e\n$s');
-      loadingState = LoadingState.fail;
-      controller.update();
-      return;
-    }
 
+      if(result.content!.length != index){
+        loadingState = LoadingState.noMoreData;
+      }
+      else{
+        loadingState = LoadingState.success;
+      }
+      controller.update();
+      return result;
+    }
   }
+
 }
