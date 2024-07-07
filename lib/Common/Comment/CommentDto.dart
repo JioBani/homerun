791bd559ef2423f1ef22f7afb2ee7826f9b6e02f
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-07 21:04:09 +0900

    feat : replyTarget 추가

--- lib/Common/Comment/CommentDto.dart
+++ lib/Common/Comment/CommentDto.dart
@@ -1,24 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CommentDto {
-  final Timestamp date;
-  final int? like;
-  final int? dislike;
   final String content;
   final String uid;
+  final Timestamp date;
+  final DocumentReference? replyTarget;
+  final int? like;
+  final int? dislike;
+
 
   CommentDto({
+    required this.content,
+    required this.uid,
     required this.date,
+    this.replyTarget,
     this.like,
     this.dislike,
-    required this.content,
-    required this.uid
   });
 
   factory CommentDto.fromMap(Map<String, dynamic> map) {
     return CommentDto(
         date: map['date'],
         like: map['like'],
+        replyTarget: map['replyTarget'],
         dislike: map['dislike'],
         content: map['content'],
         uid: map['uid']
@@ -39,16 +43,18 @@ class CommentDto {
       : date = Timestamp.now(),
         like = 0,
         dislike = 0,
+        replyTarget = null,
         content = '청년특공 어떻게 될까요? 부모님과 함께 살고 있는데도 가능할까요? 부린이라 모르는게 너무 많아요....ㅠ_ㅠ',
         uid = 'test';
 
   Map<String, dynamic> toMap() {
     return {
+      'content': content,
+      'uid' : uid,
       'date': date,
       'like': like,
       'dislike': dislike,
-      'content': content,
-      'uid' : uid
+      'replyTarget' : replyTarget,
     };
   }
 }
