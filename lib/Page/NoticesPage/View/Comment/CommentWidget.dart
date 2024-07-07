b06a481bd359df905c4205e5971e68590c4d9c09
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-07 21:10:14 +0900

    feat : Controller 및 Comment 모델 변경 사항 적용, updateLikeState 임시 주석처리

--- lib/Page/NoticesPage/View/Comment/CommentWidget.dart
+++ lib/Page/NoticesPage/View/Comment/CommentWidget.dart
@@ -2,19 +2,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:homerun/Common/Comment/Comment.dart';
 import 'package:homerun/Common/LoadingState.dart';
 import 'package:homerun/Common/StaticLogger.dart';
 import 'package:homerun/Common/TimeFormatter.dart';
 import 'package:homerun/Common/model/Result.dart';
 import 'package:homerun/Page/NoticesPage/Controller/CommentViewWidgetController.dart';
-import 'package:homerun/Page/NoticesPage/Model/Comment.dart';
-import 'package:homerun/Page/NoticesPage/Service/CommentService.dart';
 import 'package:homerun/Page/NoticesPage/View/Comment/ReplyCommentListWidget.dart';
 import 'package:homerun/Service/Auth/UserDto.dart';
 import 'package:homerun/Service/FirebaseFirestoreService.dart';
 import 'package:homerun/Style/Images.dart';
 import 'package:homerun/Style/TestImages.dart';
-import 'package:intl/intl.dart';
 import 'package:shimmer/shimmer.dart';
 
 class CommentWidget extends StatefulWidget {
@@ -29,7 +27,6 @@ class CommentWidget extends StatefulWidget {
 }
 
 class _CommentWidgetState extends State<CommentWidget> {
-  final CommentService commentService = CommentService();
   late int likeState;
   late int like;
   late int dislike;
@@ -46,9 +43,9 @@ class _CommentWidgetState extends State<CommentWidget> {
   void initState() {
     // TODO: implement initState
     getUser();
-    likeState = widget.comment.likeState;
-    like = widget.comment.commentDto.like;
-    dislike = widget.comment.commentDto.dislike;
+    likeState = widget.comment.likeState ?? 0;
+    like = widget.comment.commentDto.like ?? 0;
+    dislike = widget.comment.commentDto.dislike ?? 0;
     commentViewWidgetController = Get.find<CommentViewWidgetController>(tag: widget.noticeId);
     super.initState();
   }
@@ -58,7 +55,7 @@ class _CommentWidgetState extends State<CommentWidget> {
     setState(() {});
     Result<UserDto> userResult = await FirebaseFirestoreService.instance.getUser(widget.comment.commentDto.uid);
     if(userResult.isSuccess){
-      userDto = userResult.result!;
+      userDto = userResult.content!;
       loadingState = LoadingState.success;
     }
     else{
@@ -68,187 +65,191 @@ class _CommentWidgetState extends State<CommentWidget> {
   }
 
   Future<void> updateLikeState(int newLikeState) async {
-    final now = DateTime.now();
-    if (lastClickTime != null && now.difference(lastClickTime!) < cooldownDuration) {
-      Get.snackbar(
-        'Warning',
-        '잠시후 다시 시도해 주세요.',
-        duration: const Duration(milliseconds: 2000),
-        //backgroundColor: Theme.of(context).primaryColor
-        backgroundColor: Theme.of(context).primaryColor.withOpacity(0.5),
-        snackPosition: SnackPosition.TOP
-      );
-      return;
-    }
-
-    lastClickTime = now;
-
-    int previousLikeState = likeState;
-    int likeChange = 0;
-    int dislikeChange = 0;
-
-    Result result = await commentService.updateLikeStatus(widget.noticeId, widget.comment.commentId, newLikeState);
-
-    if (result.isSuccess) {
-      if (newLikeState == 1) {
-        likeChange = 1;
-        if (previousLikeState == 1) return;
-        if (previousLikeState == -1) dislikeChange = -1;
-      } else if (newLikeState == -1) {
-        dislikeChange = 1;
-        if (previousLikeState == 1) likeChange = -1;
-        if (previousLikeState == -1) return;
-      } else {
-        if (previousLikeState == 1) likeChange = -1;
-        if (previousLikeState == -1) dislikeChange = -1;
-      }
-
-      likeState = newLikeState;
-      like += likeChange;
-      dislike += dislikeChange;
-      setState(() {});
-    } else {
-      StaticLogger.logger.e('[CommentWidget.updateLikeState()] ${result.exception}');
-    }
+    // final now = DateTime.now();
+    // if (lastClickTime != null && now.difference(lastClickTime!) < cooldownDuration) {
+    //   Get.snackbar(
+    //     'Warning',
+    //     '잠시후 다시 시도해 주세요.',
+    //     duration: const Duration(milliseconds: 2000),
+    //     //backgroundColor: Theme.of(context).primaryColor
+    //     backgroundColor: Theme.of(context).primaryColor.withOpacity(0.5),
+    //     snackPosition: SnackPosition.TOP
+    //   );
+    //   return;
+    // }
+    //
+    // lastClickTime = now;
+    //
+    // int previousLikeState = likeState;
+    // int likeChange = 0;
+    // int dislikeChange = 0;
+    //
+    // Result result = await commentService.updateLikeStatus(widget.noticeId, widget.comment.id, newLikeState);
+    //
+    // if (result.isSuccess) {
+    //   if (newLikeState == 1) {
+    //     likeChange = 1;
+    //     if (previousLikeState == 1) return;
+    //     if (previousLikeState == -1) dislikeChange = -1;
+    //   } else if (newLikeState == -1) {
+    //     dislikeChange = 1;
+    //     if (previousLikeState == 1) likeChange = -1;
+    //     if (previousLikeState == -1) return;
+    //   } else {
+    //     if (previousLikeState == 1) likeChange = -1;
+    //     if (previousLikeState == -1) dislikeChange = -1;
+    //   }
+    //
+    //   likeState = newLikeState;
+    //   like += likeChange;
+    //   dislike += dislikeChange;
+    //   setState(() {});
+    // } else {
+    //   StaticLogger.logger.e('[CommentWidget.updateLikeState()] ${result.exception}');
+    // }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (context) {
-        if(loadingState == LoadingState.success){
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              InkWell(
-                onTap: (){
-                  Get.find<CommentViewWidgetController>(tag: widget.noticeId).setReplyMode(widget.comment.commentId);
-                },
-                child: Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(30.w),
-                      child: Image.asset(
-                        TestImages.ashe_43,
-                        width: 30.w,
-                        height: 30.w,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 6.w,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          loadingState == LoadingState.success ? userDto!.displayName! : "",
-                          style: TextStyle(fontSize: 11.sp, fontWeight: FontWeight.w600, color: const Color(0xff767676)),
+    return Container(
+      color: widget.replyTarget == null ? Theme.of(context).colorScheme.background : Colors.grey.shade300,
+      child: Builder(
+        builder: (context) {
+          if(loadingState == LoadingState.success){
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: (){
+                    //Get.find<CommentViewWidgetController>(tag: widget.noticeId).setReplyMode(widget.comment);
+                  },
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(30.w),
+                        child: Image.asset(
+                          TestImages.ashe_43,
+                          width: 30.w,
+                          height: 30.w,
                         ),
-                        Text(
-                          TimeFormatter.formatTimeDifference(widget.comment.commentDto.date.toDate()),
-                          style: TextStyle(fontSize: 11.sp, fontWeight: FontWeight.normal, color: const Color(0xff767676)),
-                        )
-                      ],
+                      ),
+                      SizedBox(
+                        width: 6.w,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            loadingState == LoadingState.success ? userDto!.displayName! : "",
+                            style: TextStyle(fontSize: 11.sp, fontWeight: FontWeight.w600, color: const Color(0xff767676)),
+                          ),
+                          Text(
+                            TimeFormatter.formatTimeDifference(widget.comment.commentDto.date.toDate()),
+                            style: TextStyle(fontSize: 11.sp, fontWeight: FontWeight.normal, color: const Color(0xff767676)),
+                          )
+                        ],
+                      ),
+                      const Expanded(child: SizedBox()),
+                      Builder(
+                          builder: (context){
+                            if(FirebaseAuth.instance.currentUser != null &&
+                                FirebaseAuth.instance.currentUser!.uid == widget.comment.commentDto.uid
+                            ){
+                              return TextButton(
+                                  onPressed: () {
+                                    commentViewWidgetController.deleteComment(widget.comment.id , widget.replyTarget);
+                                  },
+                                  child: const Text('삭제')
+                              );
+                            }
+                            else{
+                              return const SizedBox();
+                            }
+                          }
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 7.w,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 2.w),
+                  child: Text(
+                    //comment.commentDto.content,
+                    widget.comment.toMap().toString(),
+                    style: TextStyle(
+                      fontSize: 10.sp,
+                      fontWeight: FontWeight.normal,
                     ),
-                    const Expanded(child: SizedBox()),
-                    Builder(
-                        builder: (context){
-                          if(FirebaseAuth.instance.currentUser != null &&
-                              FirebaseAuth.instance.currentUser!.uid == widget.comment.commentDto.uid
-                          ){
-                            return TextButton(
-                                onPressed: () {
-                                  commentViewWidgetController.deleteComment(widget.comment.commentId , widget.replyTarget);
-                                },
-                                child: const Text('삭제')
-                            );
-                          }
-                          else{
-                            return const SizedBox();
-                          }
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CommentIconButton(
+                      imagePath: NoticePageImages.comment.good,
+                      content: like.toString(),
+                      onTap: () async {
+                        if(likeState == 1){
+                          updateLikeState(0);
                         }
-                    )
+                        else{
+                          updateLikeState(1);
+                        }
+                      },
+                      color: likeState == 1 ? Theme.of(context).primaryColor : null,
+                    ),
+                    CommentIconButton(
+                      imagePath: NoticePageImages.comment.bad,
+                      content: dislike.toString(),
+                      onTap: () async {
+                        if(likeState == -1){
+                          updateLikeState(0);
+                        }
+                        else{
+                          updateLikeState(-1);
+                        }
+                      },
+                      color: likeState == -1 ? Theme.of(context).primaryColor : null,
+                    ),
+                    CommentIconButton(
+                      imagePath: NoticePageImages.comment.reply,
+                      content: '댓글 3',
+                      color: isReplyOpen ? Theme.of(context).primaryColor : null,
+                      onTap: () {
+                        isReplyOpen = !isReplyOpen;
+                        setState(() {
+
+                        });
+                        //Get.to(CommentListViewPage());
+                      },
+                    ),
+
                   ],
                 ),
-              ),
-              SizedBox(
-                height: 7.w,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 2.w),
-                child: Text(
-                  //comment.commentDto.content,
-                  widget.comment.toMap().toString(),
-                  style: TextStyle(
-                    fontSize: 10.sp,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  CommentIconButton(
-                    imagePath: NoticePageImages.comment.good,
-                    content: like.toString(),
-                    onTap: () async {
-                      if(likeState == 1){
-                        updateLikeState(0);
+                Builder(
+                    builder: (context){
+                      if(isReplyOpen && widget.replyTarget == null){
+                        return Padding(
+                          padding: EdgeInsets.only(left: 20.w),
+                          child: ReplyCommentListWidget(noticeId: widget.noticeId,comment: widget.comment,),
+                        );
                       }
                       else{
-                        updateLikeState(1);
+                        return const SizedBox();
                       }
-                    },
-                    color: likeState == 1 ? Theme.of(context).primaryColor : null,
-                  ),
-                  CommentIconButton(
-                    imagePath: NoticePageImages.comment.bad,
-                    content: dislike.toString(),
-                    onTap: () async {
-                      if(likeState == -1){
-                        updateLikeState(0);
-                      }
-                      else{
-                        updateLikeState(-1);
-                      }
-                    },
-                    color: likeState == -1 ? Theme.of(context).primaryColor : null,
-                  ),
-                  CommentIconButton(
-                    imagePath: NoticePageImages.comment.reply,
-                    content: '댓글 3',
-                    color: isReplyOpen ? Theme.of(context).primaryColor : null,
-                    onTap: () {
-                      isReplyOpen = !isReplyOpen;
-                      setState(() {
-
-                      });
-                    },
-                  ),
-
-                ],
-              ),
-              Builder(
-                  builder: (context){
-                    if(isReplyOpen && widget.replyTarget == null){
-                      return Padding(
-                        padding: EdgeInsets.only(left: 20.w),
-                        child: ReplyCommentListWidget(noticeId: widget.noticeId,comment: widget.comment,),
-                      );
                     }
-                    else{
-                      return const SizedBox();
-                    }
-                  }
-              )
-            ],
-          );
-        }
-        else{
-          return const LoadingPlaceHolder();
-        }
+                )
+              ],
+            );
+          }
+          else{
+            return const LoadingPlaceHolder();
+          }
 
-      }
+        }
+      ),
     );
   }
 }
