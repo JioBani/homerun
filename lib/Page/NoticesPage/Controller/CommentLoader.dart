49d24d611eef83a164a2dc4d95b0e005b1687af5
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-08 00:14:57 +0900

    feat : reload 추가

--- lib/Page/NoticesPage/Controller/CommentLoader.dart
+++ lib/Page/NoticesPage/Controller/CommentLoader.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:homerun/Common/Comment/Comment.dart';
 import 'package:homerun/Common/Comment/CommentService.dart';
+import 'package:homerun/Common/Comment/Enums.dart';
 import 'package:homerun/Common/Firebase/FirestoreReferences.dart';
 import 'package:homerun/Common/LoadingState.dart';
 import 'package:homerun/Common/StaticLogger.dart';
@@ -58,6 +59,10 @@ class CommentLoader{
       controller.update();
       return result;
     }
+  }
+
+  Future<Result<List<Comment>>> reload(){
+    return getComments(comments.length , reset:true);
   }
 
 }
