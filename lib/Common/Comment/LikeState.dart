d794f31544960eb0ee5fa02a5e7c6d8df77e349a
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-07 22:30:51 +0900

    add : LikeState추가

---
+++ lib/Common/Comment/LikeState.dart
@@ -0,0 +1,11 @@
+class LikeState{
+  final int likeState;
+  final int like;
+  final int dislike;
+
+  LikeState({
+    required this.likeState,
+    required this.like,
+    required this.dislike,
+  });
+}
