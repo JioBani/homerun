a0364138dc2d27443045853ab668a1d01d3f2a67
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-09 20:10:28 +0900

    feat : 최소 높이 적용

--- lib/Page/NoticesPage/View/Comment/CommentTabChildWidget.dart
+++ lib/Page/NoticesPage/View/Comment/CommentTabChildWidget.dart
@@ -13,50 +13,55 @@ class CommentTabChildWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GetBuilder<CommentViewWidgetController>(
-            tag: noticeId,
-            builder: (controller){
-              CommentLoader commentLoader =  controller.showLoader;
-              if(commentLoader.loadingState == LoadingState.success ||
-                  commentLoader.loadingState == LoadingState.noMoreData
-              ){
-                return Column(
-                  children: commentLoader.comments.map(
-                          (comment) => Padding(
-                          padding: EdgeInsets.symmetric(vertical: 5.w),
-                          child: CommentWidget(comment: comment, noticeId: noticeId,)
-                      )
-                  ).toList(),
-                );
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: 200.w,
+      ),
+      child: Column(
+        children: [
+          GetBuilder<CommentViewWidgetController>(
+              tag: noticeId,
+              builder: (controller){
+                CommentLoader commentLoader =  controller.showLoader;
+                if(commentLoader.loadingState == LoadingState.success ||
+                    commentLoader.loadingState == LoadingState.noMoreData
+                ){
+                  return Column(
+                    children: commentLoader.comments.map(
+                            (comment) => Padding(
+                            padding: EdgeInsets.symmetric(vertical: 5.w),
+                            child: CommentWidget(comment: comment, noticeId: noticeId,)
+                        )
+                    ).toList(),
+                  );
+                }
+                else{
+                  return const SizedBox();
+                }
               }
-              else{
-                return const SizedBox();
+          ),
+          GetBuilder<CommentViewWidgetController>(
+              tag: noticeId,
+              builder: (controller){
+                CommentLoader commentLoader =  controller.showLoader;
+                if(commentLoader.loadingState == LoadingState.success){
+                  return TextButton(
+                      onPressed: (){
+                        controller.loadComment();
+                      },
+                      child: Text('더보기')
+                  );
+                }
+                else if(commentLoader.loadingState == LoadingState.noMoreData){
+                  return Text("마지막 댓글 입니다.");
+                }
+                else{
+                  return Text("댓글을 불러 올 수 없습니다.");
+                }
               }
-            }
-        ),
-        GetBuilder<CommentViewWidgetController>(
-            tag: noticeId,
-            builder: (controller){
-              CommentLoader commentLoader =  controller.showLoader;
-              if(commentLoader.loadingState == LoadingState.success){
-                return TextButton(
-                    onPressed: (){
-                      commentLoader.getComments(2);
-                    },
-                    child: Text('더보기')
-                );
-              }
-              else if(commentLoader.loadingState == LoadingState.noMoreData){
-                return Text("마지막 댓글 입니다.");
-              }
-              else{
-                return Text("댓글을 불러 올 수 없습니다.");
-              }
-            }
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
