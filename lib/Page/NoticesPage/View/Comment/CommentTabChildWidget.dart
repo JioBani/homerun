ec1f55dd261b4a93d7ec737b777840ad276c11ed
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-09 17:22:31 +0900

    feat : CommentTabChildWidget 추가

---
+++ lib/Page/NoticesPage/View/Comment/CommentTabChildWidget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:homerun/Common/LoadingState.dart';
+import 'package:homerun/Page/NoticesPage/Controller/CommentLoader.dart';
+
+import '../../Controller/CommentViewWidgetController.dart';
+import 'CommentWidget.dart';
+
+class CommentTabChildWidget extends StatelessWidget {
+  const CommentTabChildWidget({super.key, required this.noticeId});
+  final String noticeId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GetBuilder<CommentViewWidgetController>(
+            tag: noticeId,
+            builder: (controller){
+              CommentLoader commentLoader =  controller.showLoader;
+              if(commentLoader.loadingState == LoadingState.success ||
+                  commentLoader.loadingState == LoadingState.noMoreData
+              ){
+                return Column(
+                  children: commentLoader.comments.map(
+                          (comment) => Padding(
+                          padding: EdgeInsets.symmetric(vertical: 5.w),
+                          child: CommentWidget(comment: comment, noticeId: noticeId,)
+                      )
+                  ).toList(),
+                );
+              }
+              else{
+                return const SizedBox();
+              }
+            }
+        ),
+        GetBuilder<CommentViewWidgetController>(
+            tag: noticeId,
+            builder: (controller){
+              CommentLoader commentLoader =  controller.showLoader;
+              if(commentLoader.loadingState == LoadingState.success){
+                return TextButton(
+                    onPressed: (){
+                      commentLoader.getComments(2);
+                    },
+                    child: Text('더보기')
+                );
+              }
+              else if(commentLoader.loadingState == LoadingState.noMoreData){
+                return Text("마지막 댓글 입니다.");
+              }
+              else{
+                return Text("댓글을 불러 올 수 없습니다.");
+              }
+            }
+        ),
+      ],
+    );
+  }
+}
