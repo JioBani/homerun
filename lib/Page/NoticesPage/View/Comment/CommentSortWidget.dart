7a0746c3f318c1de3c0cfe62783febf006c288d9
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-10 20:44:26 +0900

    feat : 댓글 갯수 텍스트 추가

--- lib/Page/NoticesPage/View/Comment/CommentSortWidget.dart
+++ lib/Page/NoticesPage/View/Comment/CommentSortWidget.dart
@@ -1,9 +1,10 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/instance_manager.dart';
+import 'package:get/get.dart';
 import 'package:homerun/Common/Comment/Enums.dart';
 import 'package:homerun/Page/NoticesPage/Controller/CommentViewWidgetController.dart';
+import 'package:homerun/Style/Images.dart';
 import 'package:homerun/Style/Palette.dart';
 
 class CommentSortWidget extends StatefulWidget {
@@ -26,6 +27,18 @@ class _CommentSortWidgetState extends State<CommentSortWidget> {
   Widget build(BuildContext context) {
     return Row(
       children: [
+        Row(
+          children: [
+            Image.asset(NoticePageImages.comment.reply),
+            SizedBox(width: 5.w,),
+            GetBuilder<CommentViewWidgetController>(
+              tag: widget.noticeId,
+              builder: (controller) {
+                return Text("${controller.showLoader.allCommentCount}개의 댓글");
+              }
+            ),
+          ],
+        ),
         Spacer(),
         SortDropDownButton(noticeId: widget.noticeId,),
       ],
