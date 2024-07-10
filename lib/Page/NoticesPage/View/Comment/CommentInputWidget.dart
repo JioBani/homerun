e1def7cc1ad93573b02cc4b3389d8afc377375a5
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-10 19:34:04 +0900

    feat : 댓글 등록 버튼 추가

--- lib/Page/NoticesPage/View/Comment/CommentInputWidget.dart
+++ lib/Page/NoticesPage/View/Comment/CommentInputWidget.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:homerun/Common/Comment/Comment.dart';
+import 'package:homerun/Common/model/Result.dart';
+import 'package:homerun/Page/NoticesPage/Controller/CommentViewWidgetController.dart';
 import 'package:homerun/Style/Palette.dart';
 
 class CommentInputWidget extends StatefulWidget {
@@ -13,6 +18,7 @@ class CommentInputWidget extends StatefulWidget {
 
 class _CommentInputWidgetState extends State<CommentInputWidget> with TickerProviderStateMixin {
   final FocusNode _focusNode = FocusNode();
+  final TextEditingController textEditingController = TextEditingController();
 
   @override
   void initState() {
@@ -35,39 +41,88 @@ class _CommentInputWidgetState extends State<CommentInputWidget> with TickerProv
     });
   }
 
-  late final AnimationController _controller = AnimationController(
-    duration: const Duration(milliseconds: 200),
-    vsync: this,
-  );
-
-
   @override
   Widget build(BuildContext context) {
-    return  TextFormField(
-      controller: TextEditingController(),
-      cursorColor: const Color(0xFF35C5F0),
-      maxLines: _focusNode.hasFocus ? 6 : 1,
-      focusNode: _focusNode,
-      keyboardType: TextInputType.multiline,
-      decoration: InputDecoration(
-        filled: true,
-        fillColor: const Color(0x00FBFBFB),
-        hintText: ' 댓글을 입력해주세요.',
-        hintStyle: TextStyle(color: Palette.brightMode.lightText , fontSize: 14.sp),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(3.r),
-          borderSide: BorderSide(color: Palette.brightMode.lightText,width: 1.sp),
+    return  Column(
+      children: [
+        TextFormField(
+          controller: textEditingController,
+          cursorColor: const Color(0xFF35C5F0),
+          maxLines: _focusNode.hasFocus ? 6 : 1,
+          focusNode: _focusNode,
+          keyboardType: TextInputType.multiline,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: const Color(0x00FBFBFB),
+            hintText: ' 댓글을 입력해주세요.',
+            hintStyle: TextStyle(color: Palette.brightMode.lightText , fontSize: 14.sp),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(3.r),
+              borderSide: BorderSide(color: Palette.brightMode.lightText,width: 1.sp),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(3.r),
+              borderSide: BorderSide(color: Palette.brightMode.lightText,width: 1.sp),
+            ),
+            contentPadding: EdgeInsets.symmetric(vertical: 4.w, horizontal: 6.w),
+          ),
+          style: TextStyle(
+              fontSize: 14.sp,
+              color: Palette.brightMode.mediumText
+          ),
         ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(3.r),
-          borderSide: BorderSide(color: Palette.brightMode.lightText,width: 1.sp),
-        ),
-        contentPadding: EdgeInsets.symmetric(vertical: 4.w, horizontal: 6.w),
-      ),
-      style: TextStyle(
-          fontSize: 14.sp,
-          color: Palette.brightMode.mediumText
-      ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Builder(builder: (context){
+            if(_focusNode.hasFocus){
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 3.w),
+                child: SizedBox(
+                  width: 50.w,
+                  height: 25.w,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if(textEditingController.text.isEmpty){
+                        Get.snackbar('알림','내용을 입력해주세요.');
+                      }
+                      else{
+                        Result<Comment> result = await Get.find<CommentViewWidgetController>(tag: widget.noticeId)
+                            .uploadComment(textEditingController.text);
+
+                        if(result.isSuccess){
+                          Get.snackbar('알림','댓글을 달았습니다.');
+                          _focusNode.unfocus();
+                        }
+                        else{
+                          Get.snackbar('오류','댓글 업로드에 실패했습니다.');
+                        }
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        shape:  RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(3.r),
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 0 , vertical: 0)
+                    ),
+                    child: Text(
+                      "등록",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 12.sp
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }
+            else{
+              return SizedBox();
+            }
+          }),
+        )
+      ],
     );
   }
 }
