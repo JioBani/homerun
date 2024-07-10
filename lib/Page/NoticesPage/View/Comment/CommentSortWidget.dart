6e1d3debb78c31998494f19cfd867a7577447a09
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-10 20:46:55 +0900

    feat : Spacer 에 const 추가

--- lib/Page/NoticesPage/View/Comment/CommentSortWidget.dart
+++ lib/Page/NoticesPage/View/Comment/CommentSortWidget.dart
@@ -39,7 +39,7 @@ class _CommentSortWidgetState extends State<CommentSortWidget> {
             ),
           ],
         ),
-        Spacer(),
+        const Spacer(),
         SortDropDownButton(noticeId: widget.noticeId,),
       ],
     );
