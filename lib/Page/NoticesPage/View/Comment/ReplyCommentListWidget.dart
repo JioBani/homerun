9426bf610b79e8e706524c6ddbeb80b054d8cfb7
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-11 19:50:20 +0900

    feat : 댓글 더 불러오기 적용

--- lib/Page/NoticesPage/View/Comment/ReplyCommentListWidget.dart
+++ lib/Page/NoticesPage/View/Comment/ReplyCommentListWidget.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:homerun/Common/Comment/Comment.dart';
 import 'package:homerun/Common/LoadingState.dart';
+import 'package:homerun/Common/model/Result.dart';
 import 'package:homerun/Page/NoticesPage/Controller/ReplyCommentListContoller.dart';
 import 'package:homerun/Page/NoticesPage/View/Comment/CommentInputWidget.dart';
+import 'package:homerun/Page/NoticesPage/View/Comment/CommentSnackbar.dart';
 import 'package:homerun/Page/NoticesPage/View/Comment/CommentWidget.dart';
 
 class ReplyCommentListWidget extends StatefulWidget {
@@ -39,22 +42,36 @@ class _ReplyCommentListWidgetState extends State<ReplyCommentListWidget> {
         GetBuilder<ReplyCommentWidgetController>(
             tag: ReplyCommentWidgetController.makeTag(widget.noticeId, widget.comment.id),
             builder: (controller) {
-              if(controller.loadingState == LoadingState.success){
-                return Column(
-                  children: controller.replyList.map((reply) =>
-                      CommentWidget(
-                        comment: reply,
-                        noticeId: widget.noticeId ,
-                        replyTarget: widget.comment,
-                      )
-                  ).toList(),
-                );
+              return Column(
+                children: controller.replyList.map((reply) =>
+                    CommentWidget(
+                      comment: reply,
+                      noticeId: widget.noticeId ,
+                      replyTarget: widget.comment,
+                    )
+                ).toList(),
+              );
+            }
+        ),
+        GetBuilder<ReplyCommentWidgetController>(
+            tag: ReplyCommentWidgetController.makeTag(widget.noticeId, widget.comment.id),
+            builder: (controller) {
+              if(controller.loadingState == LoadingState.noMoreData){
+                return const SizedBox();
               }
-              else if(controller.loadingState == LoadingState.fail){
-                return const Text("댓글을 불러 올 수 없습니다.");
+              else if(controller.loadingState == LoadingState.loading){
+                return const CupertinoActivityIndicator();
               }
               else{
-                return const CupertinoActivityIndicator();
+                return TextButton(
+                    onPressed: () async {
+                      Result result = await controller.load();
+                      if(!result.isSuccess){
+                        CommentSnackbar.show("오류", "댓글을 불러 올 수 없습니다.");
+                      }
+                    },
+                    child: const Text("더보기")
+                );
               }
             }
         ),
