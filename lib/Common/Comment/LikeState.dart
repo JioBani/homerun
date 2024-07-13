91e37d3c248b8038e1457bd4fdfc0815f2a34b0c
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-13 18:53:05 +0900

    feat : fromMap 추가

--- lib/Common/Comment/LikeState.dart
+++ lib/Common/Comment/LikeState.dart
@@ -8,4 +8,12 @@ class LikeState{
     required this.likeChange,
     required this.dislikeChange,
   });
+
+  factory LikeState.fromMap(Map<String, dynamic> map) {
+    return LikeState(
+      likeState: map['likeState'],
+      likeChange: map['likeChange'],
+      dislikeChange: map['dislikeChange']
+    );
+  }
 }
