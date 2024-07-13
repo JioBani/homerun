f099eb32ec0e1a113abaff80cff06d37ebd059e8
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-13 21:27:06 +0900

    feat : 왼쪽 테두리 추가

--- lib/Page/NoticesPage/View/Comment/ReplyCommentListWidget.dart
+++ lib/Page/NoticesPage/View/Comment/ReplyCommentListWidget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:homerun/Common/Comment/Comment.dart';
 import 'package:homerun/Common/LoadingState.dart';
 import 'package:homerun/Common/model/Result.dart';
@@ -9,6 +9,7 @@ import 'package:homerun/Page/NoticesPage/Controller/ReplyCommentListContoller.da
 import 'package:homerun/Page/NoticesPage/View/Comment/CommentInputWidget.dart';
 import 'package:homerun/Page/NoticesPage/View/Comment/CommentSnackbar.dart';
 import 'package:homerun/Page/NoticesPage/View/Comment/CommentWidget.dart';
+import 'package:homerun/Style/Palette.dart';
 
 class ReplyCommentListWidget extends StatefulWidget {
   const ReplyCommentListWidget({super.key, required this.noticeId, required this.comment});
@@ -37,46 +38,55 @@ class _ReplyCommentListWidgetState extends State<ReplyCommentListWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GetBuilder<ReplyCommentWidgetController>(
-            tag: ReplyCommentWidgetController.makeTag(widget.noticeId, widget.comment.id),
-            builder: (controller) {
-              return Column(
-                children: controller.replyList.map((reply) =>
-                    CommentWidget(
-                      comment: reply,
-                      noticeId: widget.noticeId ,
-                      replyTarget: widget.comment,
-                    )
-                ).toList(),
-              );
-            }
-        ),
-        GetBuilder<ReplyCommentWidgetController>(
-            tag: ReplyCommentWidgetController.makeTag(widget.noticeId, widget.comment.id),
-            builder: (controller) {
-              if(controller.loadingState == LoadingState.noMoreData){
-                return const SizedBox();
-              }
-              else if(controller.loadingState == LoadingState.loading){
-                return const CupertinoActivityIndicator();
-              }
-              else{
-                return TextButton(
-                    onPressed: () async {
-                      Result result = await controller.load();
-                      if(!result.isSuccess){
-                        CommentSnackbar.show("오류", "댓글을 불러 올 수 없습니다.");
-                      }
-                    },
-                    child: const Text("더보기")
+    return Container(
+      padding: EdgeInsets.only(left: 7.5.w),
+      decoration: BoxDecoration(
+        border: Border(left: BorderSide(
+          color: Palette.brightMode.lightText,
+          width: 1.w
+        ))
+      ),
+      child: Column(
+        children: [
+          GetBuilder<ReplyCommentWidgetController>(
+              tag: ReplyCommentWidgetController.makeTag(widget.noticeId, widget.comment.id),
+              builder: (controller) {
+                return Column(
+                  children: controller.replyList.map((reply) =>
+                      CommentWidget(
+                        comment: reply,
+                        noticeId: widget.noticeId ,
+                        replyTarget: widget.comment,
+                      )
+                  ).toList(),
                 );
               }
-            }
-        ),
-        CommentInputWidget(noticeId: widget.noticeId,onFocus: (){},replyTarget: widget.comment,),
-      ],
+          ),
+          GetBuilder<ReplyCommentWidgetController>(
+              tag: ReplyCommentWidgetController.makeTag(widget.noticeId, widget.comment.id),
+              builder: (controller) {
+                if(controller.loadingState == LoadingState.noMoreData){
+                  return const SizedBox();
+                }
+                else if(controller.loadingState == LoadingState.loading){
+                  return const CupertinoActivityIndicator();
+                }
+                else{
+                  return TextButton(
+                      onPressed: () async {
+                        Result result = await controller.load();
+                        if(!result.isSuccess){
+                          CommentSnackbar.show("오류", "댓글을 불러 올 수 없습니다.");
+                        }
+                      },
+                      child: const Text("더보기")
+                  );
+                }
+              }
+          ),
+          CommentInputWidget(noticeId: widget.noticeId,onFocus: (){},replyTarget: widget.comment,),
+        ],
+      ),
     );
   }
 }
