0bc33f09c90d4f9e2029517e0ff59d8866d1d45d
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-16 23:35:06 +0900

    feat : Comment/CommentWidget , CommentInputWidget 적용

--- lib/Page/SiteReviewPage/View/SiteReview/CommentViewWidget.dart
+++ lib/Page/SiteReviewPage/View/SiteReview/CommentViewWidget.dart
@@ -1,14 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:homerun/Common/Comment/Comment.dart';
+import 'package:homerun/Common/Comment/View/CommentInputWidget.dart';
+import 'package:homerun/Common/Comment/View/CommentWidget.dart';
 import 'package:homerun/Common/LoadingState.dart';
-import 'package:homerun/Common/TimeFormatter.dart';
+import 'package:homerun/Common/model/Result.dart';
 import 'package:homerun/Page/NoticesPage/Model/SiteReview.dart';
 import 'package:homerun/Page/SiteReviewPage/Controller/CommentViewWidgetController.dart';
-import 'package:homerun/Service/Auth/AuthService.dart';
-import 'package:homerun/Style/Palette.dart';
-import 'package:homerun/Style/TestImages.dart';
 
 class CommentViewWidget extends StatelessWidget {
   const CommentViewWidget({super.key, required this.siteReview});
@@ -16,9 +15,9 @@ class CommentViewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Get.put(
-      CommentViewWidgetController(noticeId: siteReview.noticeId, reviewId: siteReview.id),
-      tag: CommentViewWidgetController.makeTag(siteReview.noticeId, siteReview.id)
+    var controller = Get.put(
+        CommentViewWidgetController(noticeId: siteReview.noticeId, reviewId: siteReview.id),
+        tag: CommentViewWidgetController.makeTag(siteReview.noticeId, siteReview.id)
     );
 
     return Column(
@@ -35,18 +34,52 @@ class CommentViewWidget extends StatelessWidget {
             ),
           ),
         ),
+        CommentInputWidget(
+          onTapSubmit: (context , content , focusNode , textController) async {
+            Result? result = await controller.commentListController.upload(content);
+            if(result != null && result.isSuccess){
+              focusNode.unfocus();
+              textController.clear();
+            }
+          },
+        ),
+        SizedBox(height: 15.w,),
+        GetBuilder<CommentViewWidgetController>(
+          tag: CommentViewWidgetController.makeTag(siteReview.noticeId, siteReview.id),
+          builder: (controller){
+            return Column(
+              children: controller.commentListController.comments.map((comment) =>
+                  CommentWidget(
+                    commentController: controller.commentListController,
+                    comment: comment,
+                  )
+              ).toList(),
+            );
+          }
+        ),
         GetBuilder<CommentViewWidgetController>(
             tag: CommentViewWidgetController.makeTag(siteReview.noticeId, siteReview.id),
             builder: (controller){
-              if(controller.loadingState == LoadingState.success){
-                return Column(
-                  children: controller.comments.map((comment) =>
-                      CommentWidget(comment: comment , siteReview: siteReview,)
-                  ).toList(),
+              if(
+                controller.commentListController.loadingState == LoadingState.success
+              ){
+                return Center(
+                  child: TextButton(
+                      onPressed: (){
+                        controller.commentListController.load(CommentViewWidgetController.commentPerLoad);
+                      },
+                      child: const Text("더보기")
+                  ),
                 );
               }
-              else{
+              else if(controller.commentListController.loadingState == LoadingState.noMoreData){
                 return const SizedBox();
+              }
+              else if(controller.commentListController.loadingState == LoadingState.loading){
+                return const Center(child: CupertinoActivityIndicator());
+              }
+              else{
+                return const Center(child: Text("데이터를 불러 올 수 없습니다."));
               }
             }
         ),
@@ -54,95 +87,3 @@ class CommentViewWidget extends StatelessWidget {
     );
   }
 }
-
-class CommentWidget extends StatelessWidget {
-  const CommentWidget({super.key, required this.comment, required this.siteReview});
-  final Comment comment;
-  final SiteReview siteReview;
-
-  @override
-  Widget build(BuildContext context) {
-
-    var controller = Get.find<CommentViewWidgetController>(
-        tag: CommentViewWidgetController.makeTag(siteReview.noticeId, siteReview.id)
-    );
-
-    return Padding(
-      padding: EdgeInsets.only(bottom: 30.w),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 2.w),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(30.w),
-              child: Image.asset(
-                TestImages.irelia_6,
-                width: 30.w,
-                height: 30.w,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          SizedBox(width: 8.w,),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      "내집은언제",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: Palette.brightMode.mediumText,
-                          fontSize: 14.sp
-                      ),
-                    ),
-                    SizedBox(width: 5.w,),
-                    Text(
-                      TimeFormatter.formatTimeDifference(comment.commentDto.date.toDate()),
-                      style: TextStyle(
-                          color: Palette.brightMode.mediumText,
-                          fontSize: 12.sp
-                      ),
-                    ),
-                    const Spacer(),
-                    Builder(builder: (context){
-                      if(Get.find<AuthService>().tryGetUser()?.uid == comment.commentDto.uid){
-                        return InkWell(
-                          onTap: (){
-                            controller.removeComment(comment);
-                            //controller.doUpdate();
-                          },
-                          child: Text(
-                            "삭제",
-                            style: TextStyle(
-                              fontSize: 12.sp,
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          )
-                        );
-                      }
-                      else{
-                        return const SizedBox();
-                      }
-                    })
-                  ],
-                ),
-                Text(
-                  comment.commentDto.content,
-                  style: TextStyle(
-                      fontSize: 12.sp,
-                      color: Palette.brightMode.darkText
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
