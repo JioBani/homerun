546ee84fddf7ea7bf9e1795585f02db8b7436d39
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-17 19:02:29 +0900

    feat : Comment/CommentListController 적용

--- lib/Page/NoticesPage/Controller/CommentViewWidgetController.dart
+++ lib/Page/NoticesPage/Controller/CommentViewWidgetController.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:homerun/Common/Comment/Controller/CommentListController.dart';
 import 'package:homerun/Common/Comment/Model/Comment.dart';
 import 'package:homerun/Common/Comment/Service/CommentService.dart';
 import 'package:homerun/Common/Comment/Model/Enums.dart';
@@ -8,163 +9,91 @@ import 'package:homerun/Common/Comment/CommentReferences.dart';
 import 'package:homerun/Common/LoadingState.dart';
 import 'package:homerun/Common/model/Result.dart';
 
-import 'CommentLoader.dart';
-
-class CommentViewWidgetController extends GetxController{
+class CommentViewWidgetController extends GetxController {
   static const int initCommentNum = 2;
   static const int loadCommentNum = 3;
 
-  late final Map<NoticeCommentType , Map<OrderType , CommentLoader>> commentLoaders;
+  late final Map<NoticeCommentType, Map<OrderType, CommentListController>> commentListControllers;
 
   final String noticeId;
   final TabController tabController;
-  late CommentLoader showLoader;
   late OrderType orderType;
   late NoticeCommentType noticeCommentType;
+  late CommentListController showingController;
 
-  //#1. 인기순
-  //#2. 최신순
-
-  CommentViewWidgetController({required this.noticeId, required this.tabController}){
-    commentLoaders = {
-      NoticeCommentType.free : {
-        //#. 자유 - 최신
-        OrderType.date : CommentLoader(
-            controller: this,
-            noticeId: noticeId,
-            commentType: NoticeCommentType.free,
-            orderType: OrderType.date
-        ),
-
-        //#. 자유 - 인기
-        OrderType.likes : CommentLoader(
-            controller: this,
-            noticeId: noticeId,
-            commentType: NoticeCommentType.free,
-            orderType: OrderType.likes
-        ),
+  CommentViewWidgetController({
+    required this.noticeId,
+    required this.tabController,
+  }) {
+    commentListControllers = {
+      NoticeCommentType.free: {
+        OrderType.date: createCommentListController(NoticeCommentType.free, OrderType.date),
+        OrderType.likes: createCommentListController(NoticeCommentType.free, OrderType.likes),
       },
-
-      NoticeCommentType.eligibility : {
-        //#. 자격 - 최신
-        OrderType.date : CommentLoader(
-            controller: this,
-            noticeId: noticeId,
-            commentType: NoticeCommentType.eligibility,
-            orderType: OrderType.date
-        ),
-
-        //#. 자격 - 인기
-        OrderType.likes : CommentLoader(
-            controller: this,
-            noticeId: noticeId,
-            commentType: NoticeCommentType.eligibility,
-            orderType: OrderType.likes
-        ),
-      }
+      NoticeCommentType.eligibility: {
+        OrderType.date: createCommentListController(NoticeCommentType.eligibility, OrderType.date),
+        OrderType.likes: createCommentListController(NoticeCommentType.eligibility, OrderType.likes),
+      },
     };
 
-    orderType = OrderType.likes;
     noticeCommentType = NoticeCommentType.eligibility;
+    orderType = OrderType.likes;
+    showingController = commentListControllers[noticeCommentType]![orderType]!;
 
-    showLoader = commentLoaders[noticeCommentType]![orderType]!;
     loadComment(reset: true);
   }
 
   @override
-  onInit(){
+  void onInit() {
     tabController.addListener(onPageChange);
     super.onInit();
   }
 
   @override
-  dispose(){
+  void dispose() {
     tabController.removeListener(onPageChange);
     super.dispose();
   }
 
-  void onPageChange(){
-    if(tabController.indexIsChanging){
-      if(tabController.index == 0){
-        noticeCommentType = NoticeCommentType.eligibility;
-      }
-      else{
-        noticeCommentType = NoticeCommentType.free;
-      }
-
-      showLoader = commentLoaders[noticeCommentType]![orderType]!;
-
-      if(showLoader.loadingState == LoadingState.before){
-        loadComment();
-      }
-      update();
-    }
-  }
-
-  void setOrderType(OrderType? orderType){
-    if(orderType != null && orderType != OrderType.none){
-      this.orderType = orderType;
-      showLoader = commentLoaders[noticeCommentType]![this.orderType]!;
-
-      if(showLoader.loadingState == LoadingState.before){
-        loadComment();
-      }
-
-      update();
-    }
-  }
-
-  Future<Result<Comment>> uploadComment(String content) async {
-    Result<Comment> result = await CommentService.instance.upload(
-      commentCollection: CommentReferences.getNoticeComment(noticeId, showLoader.commentType.name),
-      content: content,
+  CommentListController createCommentListController(NoticeCommentType type, OrderType order) {
+    return CommentListController(
+      commentCollection: CommentReferences.getNoticeComment(noticeId, type.name),
+      orderType: order,
       hasLikes: true,
+      hasReply: true,
+      onUpdate: update,
     );
+  }
 
-    if(result.isSuccess){
-      showLoader.addComment(result.content!);
+  void onPageChange() {
+    if (tabController.indexIsChanging) {
+      noticeCommentType = tabController.index == 0 ? NoticeCommentType.eligibility : NoticeCommentType.free;
+      showingController = commentListControllers[noticeCommentType]![orderType]!;
+
+      if (showingController.loadingState == LoadingState.before) {
+        loadComment();
+      }
       update();
     }
-
-    return result;
   }
 
-  Future<Result<void>> deleteComment(Comment comment)async {
-    Result<void> result = await CommentService.instance.delete(comment);
+  void setOrderType(OrderType? newOrderType) {
+    if (newOrderType != null && newOrderType != OrderType.none) {
+      orderType = newOrderType;
+      showingController = commentListControllers[noticeCommentType]![orderType]!;
 
-    if(result.isSuccess){
-      showLoader.deleteComment(comment);
+      if (showingController.loadingState == LoadingState.before) {
+        loadComment();
+      }
       update();
     }
-
-    return result;
   }
 
-  Future<Result<void>> updateComment(Comment comment , String content)async {
-    Result<void> result = await CommentService.instance.update(comment.documentSnapshot.reference , content);
-
-    if(result.isSuccess){
-      comment.commentDto.content = content;
-    }
-
-    return result;
+  Future<Result?> loadComment({bool reset = false}) {
+    return showingController.load(showingController.comments.isEmpty ? initCommentNum : loadCommentNum, reset: reset);
   }
 
-
-  Future<Result<List<Comment>>> loadComment({bool reset = false}){
-    if(showLoader.comments.isEmpty){
-      return showLoader.getComments(initCommentNum , reset: reset);
-    }
-    else{
-      return showLoader.getComments(loadCommentNum , reset: reset);
-    }
-  }
-
-  Future<Result<LikeState>> updateLikeState(Comment comment , int newLikeValue){
+  Future<Result<LikeState>> updateLikeState(Comment comment, int newLikeValue) {
     return CommentService.instance.updateLikeState(comment, newLikeValue);
-  }
-
-  Future<void> reload(){
-    return showLoader.reload();
   }
 }
