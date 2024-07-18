f3b44df828cf7bd37c1c60fd6d677273fdd5ebe6
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-18 15:22:09 +0900

    add : Snackbar 추가

---
+++ lib/Common/Widget/Snackbar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class Snackbar{
+  static void show(
+    String title,
+    String content,
+    {
+      Duration duration =  const Duration(milliseconds: 1500),
+      Duration animationDuration =  const Duration(milliseconds: 750),
+      EdgeInsets? margin
+    })
+  {
+    Get.snackbar(
+      title,
+      content,
+      snackPosition: SnackPosition.BOTTOM,
+      margin: margin ?? EdgeInsets.only(bottom: 20.w , left: 20.w , right: 20.w),
+      duration: duration,
+      animationDuration: animationDuration,
+    );
+  }
+}
