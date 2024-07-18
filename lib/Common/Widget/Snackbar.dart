55a42bf305ed7b3ea361edd14691eecc4ac35e3f
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-18 17:51:27 +0900

    feat : CustomSnackbar로 이름 변경

--- lib/Common/Widget/Snackbar.dart
+++ lib/Common/Widget/Snackbar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
-class Snackbar{
+class CustomSnackbar{
   static void show(
     String title,
     String content,
