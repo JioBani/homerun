faf54a58ab66bf646511e3e99df8e6ede000adae
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-18 19:18:01 +0900

    feat : AppbarPopupMenu추가

--- lib/Page/SiteReviewPage/View/SiteReview/SiteReviewPage.dart
+++ lib/Page/SiteReviewPage/View/SiteReview/SiteReviewPage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:homerun/Page/SiteReviewPage/Model/SiteReview.dart';
 import 'package:homerun/Page/SiteReviewPage/View/SiteReview/CommentViewWidget.dart';
 import 'package:homerun/Page/SiteReviewPage/View/SiteReview/ImageSlideWidget.dart';
+import 'package:homerun/Service/Auth/AuthService.dart';
 import 'package:homerun/Service/Auth/UserDto.dart';
 import 'package:homerun/Style/Palette.dart';
 import 'package:homerun/Style/TestImages.dart';
@@ -49,6 +51,7 @@ class _SiteReviewPageState extends State<SiteReviewPage> {
             padding: EdgeInsets.symmetric(horizontal: 5.w),
             child: const Icon(Icons.share),
           ),
+          AppbarPopupMenu(isMine: Get.find<AuthService>().tryGetUser()?.uid == widget.userDto.uid,),
           SizedBox(width: 10.w,)
         ],
       ),
@@ -149,3 +152,98 @@ class ProfileWidget extends StatelessWidget {
     );
   }
 }
+
+class AppbarPopupMenu extends StatelessWidget {
+  const AppbarPopupMenu({super.key, required this.isMine});
+  final bool isMine;
+
+  PopupMenuItem<String> buildMenuItem({
+    required String value,
+    required String name,
+    required IconData icon,
+    required Color color
+  }){
+    return PopupMenuItem<String>(
+      value: value,
+      padding: EdgeInsets.zero,
+      height: 32.h,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 6.w),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(
+              icon,
+              size: 16.sp,
+              color: color,
+              weight: 0.1,
+            ),
+            Text(
+              name,
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 12.sp,
+                  color: color
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 20.sp,
+      width: 20.sp,
+      child: PopupMenuButton<String>(
+        surfaceTintColor: Colors.white,
+        color: Colors.white,
+        padding: EdgeInsets.zero,
+        icon: Icon(
+          Icons.more_vert,
+          size: 20.sp,
+          color: Colors.black
+        ),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5.r))
+        ),
+        onSelected: (String result) async {
+
+        },
+        itemBuilder: (BuildContext buildContext) {
+          if(isMine){
+            return [
+              buildMenuItem(
+                value: "수정",
+                name: "수정하기",
+                icon: Icons.mode_edit_outline_outlined,
+                color: Colors.black,
+              ),
+              buildMenuItem(
+                  value: "삭제",
+                  name: "삭제하기",
+                  icon: Icons.delete_outlined,
+                  color: Colors.redAccent
+              ),
+            ];
+          }
+          else{
+            return [
+              buildMenuItem(
+                value: "신고",
+                name: "신고하기",
+                icon: Icons.mode_edit_outline_outlined,
+                color: Colors.black,
+              )
+            ];
+          }
+        },
+        elevation: 4,
+      ),
+    );
+  }
+}
+
+
