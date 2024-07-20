ea039594965e54f2f94fabb63993feb329fe37d6
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-20 17:52:02 +0900

    feat : defaultDialog 추가

--- lib/Common/Widget/CustomDialog.dart
+++ lib/Common/Widget/CustomDialog.dart
@@ -35,4 +35,65 @@ class CustomDialog{
       },
     );
   }
+
+  static void defaultDialog({
+    required BuildContext context,
+    required Function(BuildContext) onTap,
+    required String title,
+    required String buttonText,
+    Size? buttonSize,
+    double? height,
+    double? width,
+    TextStyle? buttonTextStyle,
+    bool closedOnTap = true,
+  }){
+    CustomDialog.show(
+        height: height,
+        width: width,
+        builder: (dialogContext){
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 14.sp,
+                  ),
+                ),
+                InkWell(
+                  onTap: ()  {
+                    if(closedOnTap && dialogContext.mounted){
+                      Navigator.pop(context);
+                    }
+                    onTap(dialogContext);
+                  },
+                  child: Container(
+                    width: buttonSize?.width ?? 75.w,
+                    height:  buttonSize?.height ?? 25.w,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.r),
+                      color: Theme.of(dialogContext).primaryColor,
+                    ),
+                    child: Center(
+                      child: Text(
+                        buttonText,
+                        style: buttonTextStyle ?? TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 12.sp,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+        context: context
+    );
+  }
 }
