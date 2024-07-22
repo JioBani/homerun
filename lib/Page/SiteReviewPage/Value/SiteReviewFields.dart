a37f7022a8bb69460d40aeaa97bd76dda8b1dbbd
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-22 15:06:55 +0900

    feat  : thumbnailImagePath 추가

--- lib/Page/SiteReviewPage/Value/SiteReviewFields.dart
+++ lib/Page/SiteReviewPage/Value/SiteReviewFields.dart
@@ -2,4 +2,5 @@ class SiteReviewFields{
   static String title = "title";
   static String content = "content";
   static String noticeId = "noticeId";
+  static String thumbnailRefPath = "thumbnailRefPath";
 }
