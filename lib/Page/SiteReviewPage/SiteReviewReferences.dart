2b9fc29f2a9bf7138d1239d18d2df05f2370da28
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-22 15:00:00 +0900

    feat : getReviewImagePath를 ,getReviewImageFoiderPath로 수정, tReviewImageDocument , getReviewThumbnailPath 추가

--- lib/Page/SiteReviewPage/SiteReviewReferences.dart
+++ lib/Page/SiteReviewPage/SiteReviewReferences.dart
@@ -1,11 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class SiteReviewReferences{
   static CollectionReference getReviewCollection(String noticeId)  {
     return FirebaseFirestore.instance.collection('site_review').doc(noticeId).collection('review');
   }
 
-  static String getReviewImagePath(String noticeId , String docId){
+  static DocumentReference getReviewDocument(String noticeId , String reviewId)  {
+    return FirebaseFirestore.instance.collection('site_review').doc(noticeId).collection('review').doc(reviewId);
+  }
+
+  static String getReviewImageFolderPath(String noticeId , String docId){
     return "site_review/$noticeId/$docId/";
+  }
+
+  static Reference getReviewImageRef(String noticeId , String docId , String fileName){
+    return FirebaseStorage.instance.ref().child('site_review/$noticeId/$docId/$fileName');
+  }
+
+  static String getReviewThumbnailPath(String noticeId , String docId , String thumbnailImageName){
+    return "site_review/$noticeId/$docId/$thumbnailImageName";
   }
 }
