2452164ef32d79c4fee1739db55acac912949edb
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-23 16:42:05 +0900

    feat : setUploadedData 적용

--- lib/Page/SiteReviewPage/View/SiteReviewWritePage/SiteReviewWritePage.dart
+++ lib/Page/SiteReviewPage/View/SiteReviewWritePage/SiteReviewWritePage.dart
@@ -39,7 +39,7 @@ class _SiteReviewWritePageState extends State<SiteReviewWritePage> {
     );
 
     if(widget.isUpdateMode){
-      controller.getUploadedImages();
+      controller.setUploadedData();
       titleController.text = widget.updateTargetReview!.title;
       contentController.text = widget.updateTargetReview!.content;
     }
