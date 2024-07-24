88c46e53565bf8c4f9839f8402fb0751b58312fb
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-24 15:14:54 +0900

    feat : 더 불러오기 버튼 추가 및 noMoreData 대응

--- lib/Page/SiteReviewPage/View/SiteReviewListPage.dart
+++ lib/Page/SiteReviewPage/View/SiteReviewListPage.dart
@@ -47,7 +47,7 @@ class _SiteReviewListPageState extends State<SiteReviewListPage> {
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.add),
+            icon: const Icon(Icons.add),
             onPressed: () {
               Get.to(SiteReviewWritePage(noticeId: widget.announcement.publicAnnouncementNumber ?? '',));
             },
@@ -58,50 +58,68 @@ class _SiteReviewListPageState extends State<SiteReviewListPage> {
         child: GetBuilder<SiteReviewListPageController>(
           tag: widget.announcement.publicAnnouncementNumber ?? '',
           builder: (controller) {
-            if(controller.loadingState.value == LoadingState.success){
-              List<Widget> widgets = [];
+            List<Widget> widgets = [];
 
-              for (int i = 0; i < controller.siteReviews.length; i += 2) {
-                if (i + 1 < controller.siteReviews.length) {
-                  widgets.add(
-                    Padding(
-                      padding: EdgeInsets.only(top: 17.w),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SiteReviewListItemWidget(siteReview: controller.siteReviews[i]),
-                          SiteReviewListItemWidget(siteReview: controller.siteReviews[i + 1]),
-                        ],
-                      ),
+            for (int i = 0; i < controller.siteReviews.length; i += 2) {
+              if (i + 1 < controller.siteReviews.length) {
+                widgets.add(
+                  Padding(
+                    padding: EdgeInsets.only(top: 17.w),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SiteReviewListItemWidget(siteReview: controller.siteReviews[i]),
+                        SiteReviewListItemWidget(siteReview: controller.siteReviews[i + 1]),
+                      ],
                     ),
-                  );
-                } else {
-                  widgets.add(
-                    Padding(
-                      padding: EdgeInsets.only(top: 17.w),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          SiteReviewListItemWidget(siteReview: controller.siteReviews[i]),
-                        ],
-                      ),
+                  ),
+                );
+              } else {
+                widgets.add(
+                  Padding(
+                    padding: EdgeInsets.only(top: 17.w),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SiteReviewListItemWidget(siteReview: controller.siteReviews[i]),
+                      ],
                     ),
-                  );
-                }
+                  ),
+                );
               }
-              return Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.w),
-                child: ListView(
-                  children: widgets,
-                ),
-              );
             }
-            else if(controller.loadingState.value == LoadingState.loading){
-              return const Center(child: CupertinoActivityIndicator());
-            }
-            else{
-              return const Center(child: Text("오류가 발생했습니다."),);
-            }
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.w),
+              child: ListView(
+                children: [
+                  ...widgets,
+                  Builder(
+                      builder: (context){
+                        if(
+                        controller.loadingState.value == LoadingState.loading ||
+                            controller.loadingState.value == LoadingState.before
+                        ){
+                          return const Center(child: CupertinoActivityIndicator());
+                        }
+                        else if(controller.loadingState.value == LoadingState.fail){
+                          return const Center(child: Text("데이터를 가져 올 수 없습니다."));
+                        }
+                        else if(controller.loadingState.value == LoadingState.noMoreData){
+                          return const SizedBox();
+                        }
+                        else{
+                          return TextButton(
+                              onPressed: (){
+                                controller.loadSiteReviews();
+                              },
+                              child: const Text("${SiteReviewListPageController.loadReviewNumber} 개 더 불러오기")
+                          );
+                        }
+                      }
+                  )
+                ],
+              ),
+            );
           }
         ),
       ),
