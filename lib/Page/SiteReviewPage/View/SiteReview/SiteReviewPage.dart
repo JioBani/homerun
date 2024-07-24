857200771170fad5eab6b2e69de91cc552f8c361
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-24 21:43:22 +0900

    feat : AppbarPopupMenu 분리

--- lib/Page/SiteReviewPage/View/SiteReview/SiteReviewPage.dart
+++ lib/Page/SiteReviewPage/View/SiteReview/SiteReviewPage.dart
@@ -171,128 +171,35 @@ class ProfileWidget extends StatelessWidget {
   }
 }
 
-class AppbarPopupMenu extends StatelessWidget {
-  const AppbarPopupMenu({super.key, required this.isMine, required this.siteReview});
-  final bool isMine;
-  final SiteReview siteReview;
-
-  PopupMenuItem<String> buildMenuItem({
-    required String value,
-    required String name,
-    required IconData icon,
-    required Color color,
-  }){
-    return PopupMenuItem<String>(
-      value: value,
-      padding: EdgeInsets.zero,
-      height: 32.h,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 6.w),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(
-              icon,
-              size: 16.sp,
-              color: color,
-              weight: 0.1,
-            ),
-            Text(
-              name,
-              style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12.sp,
-                  color: color
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+class LikeIconWidget extends StatelessWidget {
+  const LikeIconWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 20.sp,
-      width: 20.sp,
-      child: PopupMenuButton<String>(
-        surfaceTintColor: Colors.white,
-        color: Colors.white,
-        padding: EdgeInsets.zero,
-        icon: Icon(
-          Icons.more_vert,
-          size: 20.sp,
-          color: Colors.black
-        ),
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(5.r))
-        ),
-        onSelected: (String result) async {
-          if(result == "삭제"){
-            var(Result result, bool isSucess) = await LoadingDialog.showLoadingDialogWithFuture<Result>(
-                context,
-                SiteReviewService.instance.delete(siteReview)
-            );
-
-            if(result.isSuccess){
-              CustomDialog.defaultDialog(
-                  context: context,
-                  onTap: (dialogContext){
-                    if(context.mounted){
-                      Navigator.pop(context);
-                    }
-                  },
-                  title: "삭제되었습니다.",
-                  buttonText: "확인"
-              );
-
-              Get.find<SiteReviewListPageController>(tag: siteReview.noticeId).removeReview(siteReview);
-            }
-            else{
-              CustomSnackbar.show("오류" , "삭제에 실패했습니다.");
-            }
-          }
-          else if(result == "수정"){
-            Get.to(
-              SiteReviewWritePage(
-                noticeId: siteReview.noticeId,
-                updateTargetReview: siteReview,
-                isUpdateMode : true
-              )
-            );
-          }
-        },
-        itemBuilder: (BuildContext buildContext) {
-          if(isMine){
-            return [
-              buildMenuItem(
-                value: "수정",
-                name: "수정하기",
-                icon: Icons.mode_edit_outline_outlined,
-                color: Colors.black,
-              ),
-              buildMenuItem(
-                  value: "삭제",
-                  name: "삭제하기",
-                  icon: Icons.delete_outlined,
-                  color: Colors.redAccent
-              ),
-            ];
+    return GetBuilder<SiteReviewPageController>(
+        builder: (controller) {
+          if(controller.loadableLike.loadingState == LoadingState.loading){
+            return const CupertinoActivityIndicator();
           }
           else{
-            return [
-              buildMenuItem(
-                value: "신고",
-                name: "신고하기",
-                icon: Icons.mode_edit_outline_outlined,
-                color: Colors.black,
-              )
-            ];
+            if(controller.loadableLike.value == null || !controller.loadableLike.value!){
+              return InkWell(
+                child: const Icon(Icons.favorite_border),
+                onTap: (){
+                  controller.like();
+                },
+              );
+            }
+            else{
+              return InkWell(
+                child: const Icon(Icons.favorite, color: Colors.redAccent,),
+                onTap: (){
+                  controller.unlike();
+                },
+              );
+            }
           }
-        },
-        elevation: 4,
-      ),
+        }
     );
   }
 }
