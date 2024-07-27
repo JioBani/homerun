68f7fa3550b9cf3a6fc9903210b37c43c550efca
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-27 20:53:34 +0900

    add : NoticeDtoFields

---
+++ lib/Page/NoticesPage/Value/NoticeDtoFields.dart
@@ -0,0 +1,10 @@
+class NoticeDtoFields {
+  static const String info = 'info';
+  static const String noticeId = 'noticeId';
+  static const String views = 'views';
+  static const String likes = 'likes';
+  static const String scraps = 'scraps';
+  static const String houseName = 'houseName';
+  static const String applicationReceptionStartDate = 'applicationReceptionStartDate';
+  static const String recruitmentPublicAnnouncementDate = 'recruitmentPublicAnnouncementDate';
+}
