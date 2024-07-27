91b7a791848a3de8fb3effae098b9b18db617a45
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-27 23:05:17 +0900

    feat : NoticeDto 파일 위치 수정하면서 import 변경

--- lib/Page/NoticesPage/Model/Notice.dart
+++ lib/Page/NoticesPage/Model/Notice.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:homerun/Common/StaticLogger.dart';
-import 'package:homerun/Page/SiteReviewPage/Model/NoticeDto.dart';
+import 'package:homerun/Page/NoticesPage/Model/NoticeDto.dart';
 
 class Notice{
   DocumentSnapshot documentSnapshot;
