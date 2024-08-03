a6076c3a57ee550c69a2e0ec54e4d4c53344d80e
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-03 22:54:37 +0900

    feat : MenuItemWidget추가 및 임시 적용

--- lib/Page/MyPageDrawer/View/MyPageDrawer.dart
+++ lib/Page/MyPageDrawer/View/MyPageDrawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:homerun/Style/Palette.dart';
 import 'package:homerun/Style/TestImages.dart';
 
 class MyPageDrawer extends StatelessWidget {
@@ -16,6 +18,18 @@ class MyPageDrawer extends StatelessWidget {
           children: [
             SizedBox(height: 20.w,),
             const ProfileWidget(),
+            Expanded(
+              child: ListView(
+                children: [
+                  MenuItemWidget(iconData: Icons.bookmark_border_outlined, text: "스크랩", onTap: (){},),
+                  MenuItemWidget(iconData: Icons.folder_outlined, text: "공고스크랩", onTap: (){},),
+                  MenuItemWidget(iconData: Icons.assignment_turned_in_outlined, text: "청약자격진단", onTap: (){},),
+                  MenuItemWidget(iconData: Icons.settings_outlined, text: "설정", onTap: (){},),
+                  MenuItemWidget(iconData: Icons.notifications_none_outlined, text: "알림설정", onTap: (){},),
+                  MenuItemWidget(iconData: Icons.logout_outlined, text: "로그아웃", onTap: (){},),
+                ],
+              ),
+            )
           ],
         ),
       ),
@@ -54,4 +68,41 @@ class ProfileWidget extends StatelessWidget {
     );
   }
 }
+
+class MenuItemWidget extends StatelessWidget {
+  const MenuItemWidget({super.key, required this.iconData, required this.text, required this.onTap});
+  final IconData iconData;
+  final String text;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO 아이콘 및 글자 크기 회의
+    return InkWell(
+      onTap: onTap(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25.w , vertical: 5.w),
+        child: Row(
+          children: [
+            Icon(
+              iconData,
+              size: 20.sp,
+              color: Palette.brightMode.mediumText
+            ),
+            SizedBox(width: 5.w,),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 15.sp,
+                fontWeight: FontWeight.w500,
+                color: Palette.brightMode.mediumText
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
