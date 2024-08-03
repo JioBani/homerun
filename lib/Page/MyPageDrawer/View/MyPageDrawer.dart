e84d4efcb0e3404fb08b0522dc4cb6f76bcc2fa0
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-03 22:32:33 +0900

    add : MyPageDrawer

---
+++ lib/Page/MyPageDrawer/View/MyPageDrawer.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyPageDrawer extends StatelessWidget {
+  const MyPageDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.white,
+      surfaceTintColor : Colors.white,
+    );
+  }
+}
