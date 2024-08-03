48b8d191dd7e3046288f3fcfb3763b958c0ee5eb
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-03 22:39:55 +0900

    feat : 임시 ProfileWidget 생성 및 적용

--- lib/Page/MyPageDrawer/View/MyPageDrawer.dart
+++ lib/Page/MyPageDrawer/View/MyPageDrawer.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:homerun/Style/TestImages.dart';
 
 class MyPageDrawer extends StatelessWidget {
   const MyPageDrawer({super.key});
@@ -8,6 +11,47 @@ class MyPageDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: Colors.white,
       surfaceTintColor : Colors.white,
+      child: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 20.w,),
+            const ProfileWidget(),
+          ],
+        ),
+      ),
     );
   }
 }
+
+class ProfileWidget extends StatelessWidget {
+  const ProfileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //#. 프로필 이미지
+        ClipRRect(
+          borderRadius: BorderRadius.circular(60.w),
+          child: Image.asset(
+            TestImages.ashe_43,
+            width: 60.w,
+            height: 60.w,
+          ),
+        ),
+        SizedBox(height: 7.w,),
+        //#. 닉네임,
+        Text(
+          "크리스티나",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 13.sp
+          ),
+        ),
+        SizedBox(height: 20.w,),
+        const Divider(color: Color(0xff767676),)
+      ],
+    );
+  }
+}
+
