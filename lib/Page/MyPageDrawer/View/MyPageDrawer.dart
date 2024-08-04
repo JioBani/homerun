f4a90b944846c947d3cfe71176a36e57507a51ec
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-04 16:52:20 +0900

    feat : ProfileWidget 분리

--- lib/Page/MyPageDrawer/View/MyPageDrawer.dart
+++ lib/Page/MyPageDrawer/View/MyPageDrawer.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:homerun/Style/Palette.dart';
-import 'package:homerun/Style/TestImages.dart';
+
+import 'ProfileWidget.dart';
 
 class MyPageDrawer extends StatelessWidget {
   const MyPageDrawer({super.key});
@@ -33,38 +32,6 @@ class MyPageDrawer extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class ProfileWidget extends StatelessWidget {
-  const ProfileWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        //#. 프로필 이미지
-        ClipRRect(
-          borderRadius: BorderRadius.circular(60.w),
-          child: Image.asset(
-            TestImages.ashe_43,
-            width: 60.w,
-            height: 60.w,
-          ),
-        ),
-        SizedBox(height: 7.w,),
-        //#. 닉네임,
-        Text(
-          "크리스티나",
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 13.sp
-          ),
-        ),
-        SizedBox(height: 20.w,),
-        const Divider(color: Color(0xff767676),)
-      ],
     );
   }
 }
