cee8ab0a8508de8b4601e69fa8d74fee5ada2ccd
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-08 22:55:58 +0900

    feat : deleteScrap() 추가

--- lib/Page/ScapPage/Controller/ScrapPageController.dart
+++ lib/Page/ScapPage/Controller/ScrapPageController.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 import 'package:homerun/Common/LoadingState.dart';
 import 'package:homerun/Common/StaticLogger.dart';
+import 'package:homerun/Common/Widget/Snackbar.dart';
 import 'package:homerun/Common/model/Result.dart';
 import 'package:homerun/Page/NoticesPage/Model/Notice.dart';
 import 'package:homerun/Page/ScapPage/Service/ScrapService.dart';
+import 'package:homerun/Service/Auth/AuthService.dart';
 
 import '../Model/NoticeScrap.dart';
 
@@ -21,7 +23,7 @@ class ScrapPageController extends GetxController{
     }
 
     Result<List<NoticeScrap>> result = await ScrapService.instance.getScrapNotifications(
-        index: 1,
+        count: 10,
         startAfter: scarps.isEmpty ? null : scarps.last
     );
 
@@ -31,6 +33,25 @@ class ScrapPageController extends GetxController{
     }
     else{
       loadingState = LoadingState.fail;
+    }
+    update();
+  }
+
+  //#. 공고 삭제
+  //TODO 삭제 취소를 만들어야할지
+  Future<void> deleteScrap(NoticeScrap noticeScrap) async{
+    Result result = await ScrapService.instance.deleteNoticeScrap(noticeScrap);
+    if(result.isSuccess){
+      scarps.remove(noticeScrap);
+      CustomSnackbar.show("알림", "공고 스크랩을 삭제했습니다.");
+    }
+    else{
+      if(result.exception is ApplicationUnauthorizedException){
+        CustomSnackbar.show("오류", "로그인이 되어있지 않습니다.");
+      }
+      else{
+        CustomSnackbar.show("오류", "삭제에 실패했습니다.");
+      }
     }
     update();
   }
