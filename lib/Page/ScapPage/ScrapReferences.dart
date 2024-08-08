02669114b5d489d98bd39a9555117ae05aca3d73
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-08 22:37:48 +0900

    feat : getNoticeCollection추가

--- lib/Page/ScapPage/ScrapReferences.dart
+++ lib/Page/ScapPage/ScrapReferences.dart
@@ -1,3 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ScrapReferences{
-  
+  static CollectionReference getNoticeCollection(String userId){
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('scrap')
+        .doc("notice")
+        .collection('content');
+  }
 }
