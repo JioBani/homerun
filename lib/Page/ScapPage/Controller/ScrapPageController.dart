0cd91387a7024f15f09c47269b252a2f2aed47e4
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-08 22:40:27 +0900

    feat : getScraps() 추가

--- lib/Page/ScapPage/Controller/ScrapPageController.dart
+++ lib/Page/ScapPage/Controller/ScrapPageController.dart
@@ -1,5 +1,37 @@
 import 'package:get/get.dart';
+import 'package:homerun/Common/LoadingState.dart';
+import 'package:homerun/Common/StaticLogger.dart';
+import 'package:homerun/Common/model/Result.dart';
+import 'package:homerun/Page/NoticesPage/Model/Notice.dart';
+import 'package:homerun/Page/ScapPage/Service/ScrapService.dart';
+
+import '../Model/NoticeScrap.dart';
 
 class ScrapPageController extends GetxController{
+  LoadingState loadingState = LoadingState.before;
 
+  List<NoticeScrap> scarps = [];
+
+  Future<void> getScraps({bool isReset = false}) async {
+    loadingState = LoadingState.loading;
+    update();
+
+    if(isReset){
+      scarps = [];
+    }
+
+    Result<List<NoticeScrap>> result = await ScrapService.instance.getScrapNotifications(
+        index: 1,
+        startAfter: scarps.isEmpty ? null : scarps.last
+    );
+
+    if(result.isSuccess){
+      scarps.addAll(result.content!);
+      loadingState = LoadingState.success;
+    }
+    else{
+      loadingState = LoadingState.fail;
+    }
+    update();
+  }
 }
