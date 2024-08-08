efe312c887adedb14d02346bfaf5f445530c6074
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-08 22:39:45 +0900

    feat : Notice필드

--- lib/Page/ScapPage/Model/NoticeScrap.dart
+++ lib/Page/ScapPage/Model/NoticeScrap.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:homerun/Page/NoticesPage/Model/Notice.dart';
 import 'package:homerun/Page/ScapPage/Model/NoticeScrapDto.dart';
 
 class NoticeScrap{
   final DocumentSnapshot documentSnapshot;
   final NoticeScrapDto noticeScrapDto;
+  final Notice? notice;
 
-  NoticeScrap({required this.documentSnapshot, required this.noticeScrapDto});
+  NoticeScrap({required this.documentSnapshot, required this.noticeScrapDto , this.notice});
 }
