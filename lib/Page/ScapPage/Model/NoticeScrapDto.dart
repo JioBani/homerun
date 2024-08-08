eb3495717b4537764c2a8e946a213bcbf4b55f13
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-08 15:30:54 +0900

    add : NoticeScrapDto

---
+++ lib/Page/ScapPage/Model/NoticeScrapDto.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:homerun/Page/ScapPage/Value/NoticeScrapDtoFields.dart';
+
+class NoticeScrapDto{
+  final String noticeId;
+  final Timestamp date;
+
+  NoticeScrapDto({required this.noticeId, required this.date});
+
+  factory NoticeScrapDto.fromMap(Map<String,dynamic> map){
+    return NoticeScrapDto(
+        noticeId: map[NoticeScrapDtoFields.noticeId],
+        date: map[NoticeScrapDtoFields.date]
+    );
+  }
+}
