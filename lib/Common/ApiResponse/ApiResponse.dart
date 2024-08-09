ce8d16615ae472530145a53834d7ce6398266add
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-09 20:18:28 +0900

    move : ApiResponse를 Commen/ApiResponse 아래로 이동

--- lib/Common/ApiResponse/ApiResponse.dart
+++ lib/Common/ApiResponse/ApiResponse.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:homerun/Common/StaticLogger.dart';
+import 'package:http/http.dart';
 
 import 'HttpError.dart';
 
@@ -15,6 +18,20 @@ class ApiResponse<T> {
 
   factory ApiResponse.fromMap(Map<String, dynamic> map) {
     try{
+      return ApiResponse(
+        status: map['status'],
+        data: map['data'] as T?,
+        error: map['error'] != null ? HttpError.fromMap(map['error']) : null,
+      );
+    }catch(e , s){
+      StaticLogger.logger.e("[ApiResponse.fromMap()] $e\n$s");
+      rethrow;
+    }
+  }
+
+  factory ApiResponse.fromResponse(Response response) {
+    try{
+      Map<String, dynamic> map = jsonDecode(response.body);
       return ApiResponse(
         status: map['status'],
         data: map['data'] as T?,
