f86c99ef4d7e46ee2daf331fb1f3b959d3b7189a
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-09 20:30:51 +0900

    style : 주석 추가

--- lib/Common/ApiResponse/ApiResult.dart
+++ lib/Common/ApiResponse/ApiResult.dart
@@ -5,24 +5,35 @@ import 'package:http/http.dart';
 import 'ApiResponse.dart';
 
 class ApiResult<T>{
+
   final ApiResponse<T>? apiResponse;
+
   ///서버에서 에러가 발생한 경우 errors는 null 입니다.
   final Object? error;
+
   final StackTrace? stackTrace;
+
+  /// ApiResponse 제작중 파싱 에러가 발생하였는지
   final bool isParsingError;
+
+  /// 성공여부
+  /// statusCode와 통신 에러에 따라서 결정
   final bool isSuccess;
 
   ApiResult({required this.isSuccess,this.apiResponse, this.error,this.stackTrace ,this.isParsingError = false});
 
+  ///HTTP Request를 실행하고 결과를 ApiResult로 반환하는 함수
   static Future<ApiResult> handleRequest<T>(Future<Response> request) async {
+    //#. request 수행
     Response response;
     try{
       response = await request;
-    }catch(e,s){
+    }catch(e,s){ //#. request 실패시 실패 반환
       StaticLogger.logger.e("$e,$s");
       return ApiResult<T>.fromFailure(e,s);
     }
 
+    //#. ApiResponse 및 ApiResult 생성
     try{
       if(response.statusCode == 200 || response.statusCode == 300){
         return ApiResult<T>(
@@ -37,7 +48,7 @@ class ApiResult<T>{
         );
       }
     }
-    catch(e , s){
+    catch(e , s){ //#. ApiResponse 제작중 예외 발생
       if(response.statusCode == 200 || response.statusCode == 300){
         return ApiResult(
             isSuccess: true,
