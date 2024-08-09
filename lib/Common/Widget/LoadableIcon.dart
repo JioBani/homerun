c54f7452f5eebecd0ccb9fd33838eecf41d9fd86
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-09 16:20:17 +0900

    feat : LoadableIconController 추가 및 컨트롤러 기반 작동으로 수정. 주석 추가

--- lib/Common/Widget/LoadableIcon.dart
+++ lib/Common/Widget/LoadableIcon.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:homerun/Common/LoadingState.dart';
-import 'package:homerun/Common/model/Result.dart';
 
+import '../model/Result.dart';
+
+/// LoadableIcon은 비동기 로딩과 관련된 아이콘을 나타내는 위젯입니다.
+/// 이 위젯은 로딩 상태에 따라 아이콘을 변경하거나 특정 작업을 수행할 수 있습니다.
+///
+/// [T]는 아이콘이 로드할 데이터 타입입니다.
 class LoadableIcon<T> extends StatefulWidget {
+  /// LoadableIcon 생성자.
+  ///
+  /// [load] 시작시 데이터를 가져오는 함수입니다.
+  ///
+  /// [isMaintainValueOnFail]이 false이면 로딩 실패 시 값이 null로 설정됩니다.
   const LoadableIcon({
     super.key,
     required this.iconBuilder,
@@ -13,11 +23,26 @@ class LoadableIcon<T> extends StatefulWidget {
     this.onTap,
   });
 
-  final Widget Function(T? value ,LoadingState loadingState) iconBuilder;
-  final Future<Result<T>>? Function(T? currentValue)? onTap;
-  final Future<Result<T>> Function(T? currentValue) load;
+  /// 아이콘을 구성하는 빌더 함수.
+  final Widget Function(T? value, LoadingState loadingState) iconBuilder;
+
+  /// 아이콘 클릭시 실행되는 함수
+  ///
+  /// 값을 리턴하면 아이콘의 새로은 데이터 값이 됩니다.
+  final Future<T?>? Function(T? currentValue, LoadableIconController controller)? onTap;
+
+  /// 초기 로딩 작업을 수행하는 함수.
+  ///
+  /// 값을 리턴하면 아이콘의 새로은 데이터 값이 됩니다.
+  final Future<T?> Function(T? currentValue, LoadableIconController controller) load;
+
+  /// 로딩 실패 시 값을 유지할지 여부를 설정합니다.
   final bool isMaintainValueOnFail;
+
+  /// 아이콘의 너비.
   final double width;
+
+  /// 아이콘의 높이.
   final double height;
 
   @override
@@ -25,39 +50,44 @@ class LoadableIcon<T> extends StatefulWidget {
 }
 
 class LoadableIconState<T> extends State<LoadableIcon<T>> {
+  /// 현재 로딩 상태.
   LoadingState loadingState = LoadingState.loading;
+
+  /// 현재 값.
   T? value;
+
+  /// 아이콘의 상태를 관리하는 컨트롤러.
+  late final LoadableIconController _loadableIconController;
 
   @override
   void initState() {
+    _loadableIconController = LoadableIconController(this);
     load();
     super.initState();
   }
 
-  void setValue(T newValue){
+  /// 새로운 값을 설정합니다.
+  void setValue(T? newValue){
     value = newValue;
   }
 
+  /// 새로운 로딩 상태를 설정합니다.
   void setLoadingState(LoadingState newState){
     loadingState = newState;
   }
 
+  /// 위젯을 새로고침합니다.
   refresh() => setState(() {});
 
+  /// 비동기 로딩 작업을 수행합니다.
   Future<void> load() async{
+    //#. 로딩으로 변경
     loadingState = LoadingState.loading;
     setState(() {});
-    Result<T> result = await widget.load(value);
-    if(result.isSuccess){
-      loadingState = LoadingState.success;
-      value = result.content;
-    }
-    else{
-      loadingState = LoadingState.fail;
-      if(!widget.isMaintainValueOnFail){
-        value = null;
-      }
-    }
+    
+    //#. 값 가져오고 새로운 값으로 변경
+    T? newValue = await widget.load(value, _loadableIconController);
+    value = newValue;
     setState(() {});
   }
 
@@ -69,22 +99,8 @@ class LoadableIconState<T> extends State<LoadableIcon<T>> {
           loadingState = LoadingState.loading;
           setState(() {});
 
-          Result<T>? result = await widget.onTap!(value);
-
-          if(result == null){
-            return;
-          }
-
-          if(result.isSuccess){
-            loadingState = LoadingState.success;
-            value = result.content;
-          }
-          else{
-            loadingState = LoadingState.fail;
-            if(!widget.isMaintainValueOnFail){
-              value = null;
-            }
-          }
+          T? newValue = await widget.onTap!(value, _loadableIconController);
+          value = newValue;
           setState(() {});
         }
       },
@@ -93,10 +109,72 @@ class LoadableIconState<T> extends State<LoadableIcon<T>> {
           SizedBox(
             width: widget.width,
             height: widget.height,
-            child: widget.iconBuilder(value , loadingState)
+            child: widget.iconBuilder(value, loadingState),
           ),
         ],
       ),
     );
+  }
+}
+
+/// LoadableIcon의 상태를 제어하는 컨트롤러 클래스.
+class LoadableIconController<T> {
+  final LoadableIconState<T> loadableIconState;
+
+  LoadableIconController(this.loadableIconState);
+
+  /// 아이콘의 값을 설정합니다.
+  void setValue(T? value) {
+    loadableIconState.setValue(value);
+  }
+
+  /// 로딩 상태를 설정합니다.
+  void setLoadingState(LoadingState newState) {
+    loadableIconState.setLoadingState(newState);
+  }
+
+  /// 위젯을 새로고침합니다.
+  void setState() {
+    loadableIconState.refresh();
+  }
+
+  /// 로딩 상태를 성공으로 설정합니다.
+  void success() {
+    setLoadingState(LoadingState.success);
+  }
+
+  /// 로딩 상태를 실패로 설정합니다.
+  void failure() {
+    setLoadingState(LoadingState.fail);
+  }
+
+  /// Result 객체로부터 상태를 변경하고 값을 반환합니다.
+  T? fromResult(Result<T> result) {
+    if (result.isSuccess) {
+      success();
+      return result.content;
+    } else {
+      failure();
+      if (!loadableIconState.widget.isMaintainValueOnFail) {
+        return null;
+      } else {
+        return loadableIconState.value;
+      }
+    }
+  }
+
+  /// Result와 새로운 값으로부터 상태를 변경하고 값을 반환합니다.
+  T? fromResultAndValue(Result<T> result, T? value) {
+    if (result.isSuccess) {
+      success();
+      return value;
+    } else {
+      failure();
+      if (!loadableIconState.widget.isMaintainValueOnFail) {
+        return null;
+      } else {
+        return loadableIconState.value;
+      }
+    }
   }
 }
