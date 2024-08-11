693508777d7704b801960d7c9430c5e1d9937c8b
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-11 17:59:21 +0900

    feat : SelectBoxController를 SelectBoxWidget.dart 로 이동

--- lib/Page/LoginPage/View/UserInfoInputPage/SelectBoxWidget.dart
+++ lib/Page/LoginPage/View/UserInfoInputPage/SelectBoxWidget.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gradient_borders/gradient_borders.dart';
 import 'package:homerun/Common/Widget/Snackbar.dart';
-import 'package:homerun/Page/LoginPage/Controller/UserInfoPageController.dart';
 import 'package:homerun/Style/Palette.dart';
 import 'package:gap/gap.dart';
 
@@ -145,5 +144,51 @@ class SelectBoxWidgetState<T> extends State<SelectBoxWidget<T>> {
         ),
       ),
     );
+  }
+}
+
+class SelectBoxController<T>{
+  /// 선택한 값
+  T? value;
+
+  /// 복수선택 가능시 선택한 값들
+  List<T> values = [];
+
+  /// 버튼 위젯들
+  List<SelectBoxWidgetState<T>> widgets = [];
+
+  /// 복수 선택 가능한지
+  final bool isCanSelectMulti;
+
+  SelectBoxController({this.isCanSelectMulti = false});
+
+  /// 위젯 추가
+  void addWidget(SelectBoxWidgetState<T> widget){
+    widgets.add(widget);
+  }
+
+  /// 버튼 클릭시
+  void onTap(SelectBoxWidgetState<T> widget, bool select){
+    if(select){
+      if(isCanSelectMulti){
+        values.add(widget.widget.value);
+      }
+      else{
+        value = widget.widget.value;
+        for (var element in widgets) {
+          if(element.widget.value != value){
+            element.changeSelect(false);
+          }
+        }
+      }
+    }
+    else{
+      if(isCanSelectMulti){
+        values.remove(widget.widget.value);
+      }
+      else{
+        value = null;
+      }
+    }
   }
 }
