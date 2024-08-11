6255b9d65af335bf4045352a837625202e41bc90
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-11 18:21:11 +0900

    feat : setProfileImage() 추가

--- lib/Page/LoginPage/Controller/UserInfoPageController.dart
+++ lib/Page/LoginPage/Controller/UserInfoPageController.dart
@@ -1,5 +1,35 @@
-import '../View/UserInfoInputPage/SelectBoxWidget.dart';
+import 'package:get/get.dart';
+import 'package:homerun/Common/Widget/Snackbar.dart';
+import 'package:image_picker/image_picker.dart';
 
-class UserInfoPageController{
+class UserInfoPageController extends GetxController{
+  XFile? profileImage;
 
+  final ImagePicker picker = ImagePicker();
+  final double maxSizeMb = 3;
+
+  /// 프로필 이미지 추가
+  Future<void> setProfileImage() async {
+    try{
+      final XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+      //#. 이미지 가져왔는지 확인
+      if(pickedFile == null){
+        CustomSnackbar.show('오류', '이미지를 가져 올 수 없습니다.');
+        return;
+      }
+
+      //#. 이미지 크기 제한 확인
+      if(await pickedFile.length() / (1024 * 1024) > maxSizeMb){
+        CustomSnackbar.show('오류', '프로필 이미지의 크기는 ${maxSizeMb}MB를 넘을 수 없습니다.');
+        return;
+      }
+
+      profileImage = pickedFile;
+
+      update();
+    }catch(e,s){
+      CustomSnackbar.show('오류', '이미지를 가져 올 수 없습니다.');
+    }
+  }
 }
