8650bf8d8db4ff935bee998637980132b1ace543
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-12 20:28:29 +0900

    feat : LoginService implement

--- lib/Service/Auth/KakaoLoginService.dart
+++ lib/Service/Auth/KakaoLoginService.dart
@@ -1,39 +1,66 @@
 import 'package:homerun/Common/StaticLogger.dart';
+import 'package:homerun/Common/model/Result.dart';
+import 'package:homerun/Service/Auth/SocialLoginService.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
-class KakaoLoginService {
-  Future<(OAuthToken, User)> signIn() async {
-    try {
-      bool isInstalled = await isKakaoTalkInstalled();
-      if (isInstalled) {
-        try {
+class KakaoLoginService implements SocialLoginService{
+  //TODO 소셜로그인 서비스의 예외 객체를 사용할지 결정
+  OAuthToken? oAuthToken;
+  User? user;
+
+  @override
+  Future<Result<void>> login() async {
+    return Result.handleFuture<void>(
+      timeout: const Duration(minutes: 10),
+      action: () async {
+        //#. 카카오 설치 체크
+        bool isInstalled = await isKakaoTalkInstalled();
+
+        if (isInstalled) { //#. 설치된 경우
+
+          //#. 카카오톡으로부터 로그인
           var token = await UserApi.instance.loginWithKakaoTalk();
           var user = await UserApi.instance.me();
-          StaticLogger.logger.i(""
+
+          StaticLogger.logger.i(
               "${user.kakaoAccount?.profile?.profileImageUrl} ,"
-              "${user.kakaoAccount?.profile?.nickname}");
-          return (token, user);
-        } catch (e) {
-          StaticLogger.logger.e(e);
-          rethrow;
-        }
-      } else {
-        try {
+              "${user.kakaoAccount?.profile?.nickname}"
+          );
+
+          //#. 로그인 결과 저장
+          oAuthToken = token;
+          this.user = user;
+        } else { //#. 설치되지 않은 경우
+
+          //#. 아이디로 로그인
           var token = await UserApi.instance.loginWithKakaoAccount();
           var user = await UserApi.instance.me();
-          StaticLogger.logger.i(""
+
+          StaticLogger.logger.i(
               "${user.kakaoAccount?.profile?.profileImageUrl} ,"
-              "${user.kakaoAccount?.profile?.nickname}");
-          return (token, user);
-        } catch (e) {
-          StaticLogger.logger.e(e);
-          StaticLogger.logger.e(e);
-          rethrow;
+              "${user.kakaoAccount?.profile?.nickname}"
+          );
+
+          //#. 로그인 결과 저장
+          oAuthToken = token;
+          this.user = user;
         }
       }
-    } catch (e) {
-      StaticLogger.logger.e(e);
-      rethrow;
-    }
+    );
+  }
+
+  @override
+  Future<Result<String>> getAccessToken() async{
+    return Result.handleFuture<String>(
+      timeout: const Duration(seconds: 30),
+      action: ()async{
+        if(oAuthToken == null){
+          throw SocialServiceUnauthorizedException();
+        }
+        else{
+          return oAuthToken!.accessToken;
+        }
+      }
+    );
   }
 }
--- lib/Service/Auth/NaverLoginService.dart
+++ lib/Service/Auth/NaverLoginService.dart
@@ -1,15 +1,34 @@
 import 'package:flutter_naver_login/flutter_naver_login.dart';
-import 'package:homerun/Common/StaticLogger.dart';
+import 'package:homerun/Common/model/Result.dart';
 
-class NaverLoginService{
-  Future<NaverLoginResult> signIn() async {
-    try{
-      NaverLoginResult _result = await FlutterNaverLogin.logIn();
-      StaticLogger.logger.i("[NaverSignInService.signIn()] 로그인 성공 (id = ${_result.account.id})");
-      return _result;
-    }catch(e,s){
-      StaticLogger.logger.e('[NaverSignInService.signIn()] $e\n$s');
-      rethrow;
-    }
+import 'SocialLoginService.dart';
+
+
+class NaverLoginService implements SocialLoginService{
+  @override
+  Future<Result<void>> login() async {
+    return Result.handleFuture<void>(
+        timeout: const Duration(minutes: 10),
+        action: () async {
+          NaverLoginResult result = await FlutterNaverLogin.logIn();
+          if(result.status == NaverLoginStatus.error){
+            throw LoginFailureException(result.errorMessage);
+          }
+          else if(result.status == NaverLoginStatus.cancelledByUser){
+            throw LoginCancelException();
+          }
+        }
+    );
+  }
+
+  @override
+  Future<Result<String>> getAccessToken() async{
+    return Result.handleFuture<String>(
+        timeout: const Duration(seconds: 30),
+        action: ()async{
+          return (await FlutterNaverLogin.currentAccessToken).accessToken;
+        }
+    );
   }
 }
+
