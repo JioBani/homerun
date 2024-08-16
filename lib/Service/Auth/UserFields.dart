e4cbe81e186f945c8e319eec5aa11c81f3189165
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-16 20:44:12 +0900

    add : UserFields

---
+++ lib/Service/Auth/UserFields.dart
@@ -0,0 +1,7 @@
+class UserFields{
+  static String socialProvider = "social_provider";
+  static String displayName = "displayName";
+  static String gender = "gender";
+  static String ageRange = "ageRange";
+  static String interestedRegions = "interestedRegions";
+}
