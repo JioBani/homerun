c63fb2c32b15167f74365caa1a6904875c239099
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-17 21:45:21 +0900

    comment : 전체적인 주석 추가

--- lib/Page/AnnouncementPage/Controller/AnnouncementPageController.dart
+++ lib/Page/AnnouncementPage/Controller/AnnouncementPageController.dart
@@ -7,9 +7,14 @@ import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 class AnnouncementPageController{
   
   CollectionReference announcementCollection = FirebaseFirestore.instance.collection('announcement');
-  final int loadingNumber = 2;
 
+  /// 한번에 불러올 공지사항 수
+  final int loadingNumber = 10;
+
+  /// 공지사항 리스트
   List<Announcement> announcementList = [];
+  
+  /// 페이지 컨트롤러
   final PagingController<int, Announcement> pagingController = PagingController(firstPageKey: 0);
 
   AnnouncementPageController(){
@@ -18,19 +23,25 @@ class AnnouncementPageController{
     });
   }
 
+  /// 공지사항 불러오기
   Future<void> loadAnnouncements(int pageKey) async {
+    
+    //#. 날짜순
     Query query = announcementCollection.orderBy(AnnouncementDtoFields.date ,descending: true);
 
+    //#. 불러 온 공지사항이 있으면, 그 공지사항 이후로
     if(announcementList.isNotEmpty){
-      query = query.startAfterDocument(announcementList.first.documentSnapshot);
+      query = query.startAfterDocument(announcementList.last.documentSnapshot);
     }
 
+    //#. 불러올 개수 설정
     query = query.limit(loadingNumber);
 
     try{
       List<Announcement> newList = Announcement.makeList(await query.get());
 
       if(newList.length != loadingNumber){
+        //#. 요청 한 것 보다 적게 왔으면 noMoreData
         pagingController.appendLastPage(newList);
       }
       else{
