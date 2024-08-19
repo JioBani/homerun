8a66b0aa75ed0be35aa39356e33b36095c589ab3
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-19 21:14:16 +0900

    fix : socialProvider 값 변경

--- lib/Service/Auth/UserFields.dart
+++ lib/Service/Auth/UserFields.dart
@@ -1,6 +1,6 @@
 class UserFields{
   static String uid = "uid";
-  static String socialProvider = "social_provider";
+  static String socialProvider = "socialProvider";
   static String displayName = "displayName";
   static String gender = "gender";
   static String ageRange = "ageRange";
