79ee99dbe745ad1751f9cbf74a25392943f75090
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-22 20:29:00 +0900

    feat : 서비스 key 환경변수로 이동

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:homerun/Common/StaticLogger.dart';
+import 'package:homerun/Page/LoginPage/View/LoginPage.dart';
 import 'package:homerun/Service/Auth/AuthService.dart';
 import 'package:homerun/Service/FirebaseFirestoreService.dart';
+import 'package:homerun/Service/NaverGeocodeService/NaverGeocodeService.dart';
+import 'package:homerun/Service/NaverGeocodeService/ServiceKey.dart';
 import 'package:homerun/Style/Fonts.dart';
 import 'package:homerun/Style/MaterialTheme.dart';
 import 'package:homerun/View/HomePage/home_page.dart';
@@ -15,14 +19,26 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  KakaoSdk.init(nativeAppKey: 'c16877ed41cb3c133854a81a6ce4e980');
+
+  await dotenv.load(fileName: 'assets/config/.env');
+
+  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']);
+
   await NaverMapSdk.instance.initialize(
-    clientId: "dtcofwiywj",
+    clientId: dotenv.env['NAVER_MAP_CLIENT_ID'],
     onAuthFailed: (ex) {
       StaticLogger.logger.e("[main] 네이버 맵 인증 오류 : $ex");
     }
   );
 
+  try{
+    NaverGeocodeService.instance.init(
+        dotenv.env['NAVER_MAP_CLIENT_ID']!,
+        dotenv.env['NAVER_MAP_CLIENT_SECRET']!
+    );
+  }catch(e){
+    StaticLogger.logger.e("NaverGeocodeService 초기화 실패 : $e");
+  }
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
