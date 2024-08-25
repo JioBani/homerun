eea9693e62b9fddfdc0274250a308a29b890753a
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-25 19:31:46 +0900

    feat : 로그인 로그아웃 상태에 따라 아이콘 버튼 활성화 적용

--- lib/Page/MyPageDrawer/View/MyPageDrawer.dart
+++ lib/Page/MyPageDrawer/View/MyPageDrawer.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:homerun/Page/AnnouncementPage/View/AnnouncementPage.dart';
 import 'package:homerun/Page/ScapPage/View/ScrapPage.dart';
 import 'package:homerun/Service/Auth/AuthService.dart';
+import 'package:homerun/Service/Auth/UserDto.dart';
 import 'package:homerun/Style/Palette.dart';
 
 import 'ProfileWidget.dart';
 
+//TODO 아이콘 변경
 class MyPageDrawer extends StatelessWidget {
   const MyPageDrawer({super.key});
 
@@ -19,21 +22,57 @@ class MyPageDrawer extends StatelessWidget {
       child: SafeArea(
         child: Column(
           children: [
-            SizedBox(height: 20.w,),
             const ProfileWidget(),
+            const Divider(),
+            Gap(8.h),
             Expanded(
-              child: ListView(
-                children: [
-                  MenuItemWidget(iconData: Icons.bookmark_border_outlined, text: "스크랩", onTap: (){},),
-                  MenuItemWidget(iconData: Icons.folder_outlined, text: "공고스크랩", onTap: (){
-                    AuthService.runWithAuthCheck(()=>Get.to(const ScrapPage()));
-                  },),
-                  MenuItemWidget(iconData: Icons.assignment_turned_in_outlined, text: "청약자격진단", onTap: (){},),
-                  MenuItemWidget(iconData: Icons.settings_outlined, text: "설정", onTap: (){},),
-                  MenuItemWidget(iconData: Icons.notifications_none_outlined, text: "알림설정", onTap: (){},),
-                  MenuItemWidget(iconData: Icons.logout_outlined, text: "로그아웃", onTap: (){},),
-                  MenuItemWidget(iconData: Icons.notifications_none_outlined, text: "공지사항", onTap: ()=>Get.to(const AnnouncementPage())),
-                ],
+              child: GetX<AuthService>(
+                builder: (service) {
+                  bool isLoggedIn = service.userDto.value != null;
+                  return ListView(
+                    children: [
+                      MenuItemWidget(
+                        iconData: Icons.folder_outlined,
+                        text: "공고스크랩",
+                        onTap: ()=>AuthService.runWithAuthCheck(()=>Get.to(const ScrapPage())),
+                        isActive: isLoggedIn,
+                      ),
+                      MenuItemWidget(
+                        iconData: Icons.assignment_turned_in_outlined,
+                        text: "청약자격진단",
+                        onTap: (){},
+                        isActive: isLoggedIn,
+                      ),
+                      MenuItemWidget(
+                        iconData: Icons.settings_outlined,
+                        text: "설정",
+                        onTap: (){},
+                      ),
+                      MenuItemWidget(
+                          iconData: Icons.notifications_none_outlined,
+                          text: "공지사항",
+                          onTap: ()=>Get.to(const AnnouncementPage())
+                      ),
+                      MenuItemWidget(
+                        iconData: Icons.notifications_none_outlined,
+                        text: "알림설정",
+                        onTap: (){},
+                        isActive: isLoggedIn,
+                      ),
+                      MenuItemWidget(
+                        iconData: Icons.call,
+                        text: "광고문의",
+                        onTap: (){},
+                      ),
+                      MenuItemWidget(
+                        iconData: Icons.logout_outlined,
+                        text: "로그아웃",
+                        onTap: (){Get.find<AuthService>().logout();},
+                        isActive: isLoggedIn,
+                      ),
+                    ],
+                  );
+                }
               ),
             )
           ],
@@ -44,17 +83,26 @@ class MyPageDrawer extends StatelessWidget {
 }
 
 class MenuItemWidget extends StatelessWidget {
-  const MenuItemWidget({super.key, required this.iconData, required this.text, required this.onTap});
+  const MenuItemWidget({
+    super.key,
+    required this.iconData,
+    required this.text,
+    required this.onTap,
+    this.isActive = true,
+  });
   final IconData iconData;
   final String text;
   final Function onTap;
+  final bool isActive;
 
   @override
   Widget build(BuildContext context) {
     //TODO 아이콘 및 글자 크기 회의
     return InkWell(
       onTap: (){
-        onTap();
+        if(isActive){
+          onTap();
+        }
       },
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 25.w , vertical: 5.w),
@@ -63,7 +111,7 @@ class MenuItemWidget extends StatelessWidget {
             Icon(
               iconData,
               size: 20.sp,
-              color: Palette.brightMode.mediumText
+              color: isActive ? Palette.brightMode.mediumText : Palette.brightMode.lightText
             ),
             SizedBox(width: 5.w,),
             Text(
@@ -71,7 +119,7 @@ class MenuItemWidget extends StatelessWidget {
               style: TextStyle(
                 fontSize: 15.sp,
                 fontWeight: FontWeight.w500,
-                color: Palette.brightMode.mediumText
+                color: isActive ? Palette.brightMode.mediumText : Palette.brightMode.lightText
               ),
             )
           ],
