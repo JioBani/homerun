3ca6171241a787ecc66ffe061cbd67c43d028e10
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-09-03 16:26:55 +0900

    feat : ageRange 삭제하고 birth 추가, nullable 필드를 null 가능하지 않게 변경

--- lib/Service/Auth/UserDto.dart
+++ lib/Service/Auth/UserDto.dart
@@ -1,6 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:homerun/Common/enum/Gender.dart';
 import 'package:homerun/Service/Auth/SocialProvider.dart';
-import 'package:homerun/Value/AgeRange.dart';
 import 'package:homerun/Value/Region.dart';
 
 import 'UserFields.dart';
@@ -8,18 +8,18 @@ import 'UserFields.dart';
 class UserDto {
   final String uid;
   final SocialProvider socialProvider;
-  final String? displayName;
-  final Gender? gender;
+  final String displayName;
+  final Gender gender;
   final List<Region?> interestedRegions;
-  final AgeRange? ageRange;
+  final Timestamp birth;
 
   UserDto({
     required this.socialProvider,
     required this.uid,
     required this.displayName,
     required this.gender,
-    required this.ageRange,
     required this.interestedRegions,
+    required this.birth,
   });
 
   factory UserDto.fromMap(Map<String, dynamic> map) {
@@ -28,8 +28,8 @@ class UserDto {
       socialProvider: SocialProviderExtension.fromString(map[UserFields.socialProvider]),
       displayName: map[UserFields.displayName] as String,
       gender: GenderExtension.fromString(map[UserFields.gender] as String),
-      ageRange: AgeRange.fromString(map[UserFields.ageRange] as String),
       interestedRegions: List<String>.from(map[UserFields.interestedRegions] as List).map((region) => Region.fromString(region)).toList(),
+      birth: map[UserFields.birth] as Timestamp,
     );
   }
 
@@ -38,9 +38,9 @@ class UserDto {
       UserFields.socialProvider: socialProvider.name,
       UserFields.uid: uid,
       UserFields.displayName: displayName,
-      UserFields.gender: gender?.name,
-      UserFields.ageRange: ageRange?.name,
+      UserFields.gender: gender.name,
       UserFields.interestedRegions: interestedRegions,
+      UserFields.birth: birth,
     };
   }
 }
