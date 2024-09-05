a2830392c53d54035460777f74f3ab7a62ff8fc3
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-09-05 23:54:35 +0900

    feat : checkNickName() 추가

--- lib/Service/Auth/UserInfoService.dart
+++ lib/Service/Auth/UserInfoService.dart
@@ -1,13 +1,17 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/instance_manager.dart';
+import 'package:homerun/Common/ApiResponse/ApiResult.dart';
 import 'package:homerun/Common/StaticLogger.dart';
 import 'package:homerun/Common/model/Result.dart';
+import 'package:homerun/Security/FirebaseFunctionEndpoints.dart';
 import 'package:homerun/Service/Auth/AuthService.dart';
 import 'package:homerun/Service/Auth/UserDto.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 class UserInfoService{
 
@@ -50,5 +54,34 @@ class UserInfoService{
 
       await uploadTask.whenComplete(() {});
     });
+  }
+
+  /// 닉네임 확인
+  /// 
+  /// [returns] 사용 할 수 있는 닉네임이면 [null], 사용 할 수 없는 닉네임이면 [String] 메세지를 반환
+  Future<String?> checkNickName(String nickName) async {
+    final ApiResult<String> apiResult = await ApiResult.handleRequest<String>(
+        http.post(Uri.parse(FirebaseFunctionEndpoints.checkDisplayName),
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: jsonEncode({
+              "displayName" : nickName
+            })
+        ),
+        timeout: const Duration(minutes: 1)
+    );
+
+    if(apiResult.parsingFailed){
+      return "서버에 연결 할 수 없습니다.";
+    }
+    else{
+      if(apiResult.isSuccess){
+        return null;
+      }
+      else{
+        return apiResult.apiResponse!.error!.message;
+      }
+    }
   }
 }
