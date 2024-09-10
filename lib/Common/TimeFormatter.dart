c30deb212349e719d74d91a9c86e7f2c41f88a16
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-09-10 20:21:01 +0900

    feat : calculateDaysDifference(), dateToKoreanString(), tryDateToKoreanString() 추가

--- lib/Common/TimeFormatter.dart
+++ lib/Common/TimeFormatter.dart
@@ -2,6 +2,7 @@ import 'package:intl/intl.dart';
 
 class TimeFormatter {
   final DateFormat _datFormat = DateFormat('yyyy.MM.dd');
+  final DateFormat _koreanFormat = DateFormat('yyyy년 MM월 dd일');
 
   String formatTimeDifference(DateTime dateTime) {
     final now = DateTime.now();
@@ -18,11 +19,33 @@ class TimeFormatter {
     }
   }
 
+  /// 남은 일을 반환하는 함수
+  ///
+  /// 일 단위로 계산(시간은 무시하고 정각으로만 계산함)
+  int calculateDaysDifference(DateTime dateTime) {
+    final now = DateTime.now();
+    DateTime noonTargetDate = DateTime(dateTime.year, dateTime.month, dateTime.day, 0, 0, 0);
+    DateTime nowTriggerDate = DateTime(now.year, now.month, now.day, 0, 0, 0);
+    final difference = noonTargetDate.difference(nowTriggerDate);
+
+    return difference.inDays;
+  }
+
   datStringToTime(String string){
     return _datFormat.parseStrict(string);
   }
 
   dateToDatString(DateTime dateTime){
     return _datFormat.format(dateTime);
+  }
+
+  ///00년 00월 00일
+  dateToKoreanString(DateTime dateTime){
+    return _koreanFormat.format(dateTime);
+  }
+
+  ///00년 00월 00일
+  tryDateToKoreanString(DateTime? dateTime){
+    return dateTime == null ? null  : _koreanFormat.format(dateTime);
   }
 }
