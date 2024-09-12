02f55c0087b7d3ff2aa5a9b3f8222e7c3c532d70
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-09-12 17:18:23 +0900

    feat : 분양가 정보 데이터 연결

--- lib/Page/NoticesPage/View/NoticePage/PriceInfoWidget.dart
+++ lib/Page/NoticesPage/View/NoticePage/PriceInfoWidget.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
+import 'package:homerun/Common/PriceFormatter.dart';
+import 'package:homerun/Page/NoticesPage/Model/NoticeDto.dart';
 import 'package:homerun/Page/NoticesPage/View/NoticePage/ContentBoxWidget.dart';
 import 'package:homerun/Page/NoticesPage/View/NoticePage/InfoBoxWidget.dart';
+import 'package:homerun/Service/APTAnnouncementApiService/AptAnnouncementByHouseType.dart';
+import 'package:homerun/String/APTAnnouncementStrings.dart';
 import 'package:homerun/Style/Palette.dart';
 
 import 'SubTitleWidget.dart';
 
-//TODO 디자인 수정 및 실제 데이터 가져와야함
-//TODO 데이터 취합중일때 반응형 표현
 class PriceInfoWidget extends StatelessWidget {
-  const PriceInfoWidget({super.key});
+  const PriceInfoWidget({super.key, this.noticeDto});
+  final NoticeDto? noticeDto;
+
+  Widget makePriceListWidget(){
+    if(noticeDto == null){
+      return SizedBox(
+        height: 80.w,
+        child: const Center(child: Text(APTAnnouncementStrings.couldNotGetData)),
+      );
+    }
+    else if(noticeDto?.aptAnnouncementByTypeList == null){
+      return SizedBox(
+        height: 80.w,
+        child: const Center(child: Text(APTAnnouncementStrings.couldNotGetData)),
+      );
+    }
+    else{
+      return Column(
+        children: noticeDto!.aptAnnouncementByTypeList!.map((e)=>
+          PriceItemWidget(aptAnnouncementByHouseType : e)
+        ).toList(),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +44,14 @@ class PriceInfoWidget extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Gap(17.w),
+            //#. 소제목
             SubTitleWidget(
               text: "분양가",
               leftPadding: 17.w,
               rightPadding: 20.w,
             ),
             Gap(13.w),
+            //#. 분양가 상한제 안내 및 공급금액
             Row(
               children: [
                 Gap(25.w),
@@ -54,6 +81,7 @@ class PriceInfoWidget extends StatelessWidget {
               ],
             ),
             Gap(12.w),
+            //#. 항목
             Row(
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -64,67 +92,74 @@ class PriceInfoWidget extends StatelessWidget {
                   margin: EdgeInsets.all(1.w),
                 ),
                 ContentTitleBoxWidget(
-                  title: "공급규모",
+                  title: "분양가(최고가 기준)",
                   width: 120.w,
                   height: 30.w,
                   margin: EdgeInsets.all(1.w),
                 ),
               ],
             ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ContentTextBoxWidget(
-                  text: "084.9958A",
-                  width: 120.w,
-                  height: 30.w,
-                  margin: EdgeInsets.all(1.w),
-                ),
-                ContentTextBoxWidget(
-                  text: "084.9958A",
-                  width: 120.w,
-                  height: 30.w,
-                  margin: EdgeInsets.all(1.w),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ContentTextBoxWidget(
-                  text: "084.9958A",
-                  width: 120.w,
-                  height: 30.w,
-                  margin: EdgeInsets.all(1.w),
-                ),
-                ContentTextBoxWidget(
-                  text: "084.9958A",
-                  width: 120.w,
-                  height: 30.w,
-                  margin: EdgeInsets.all(1.w),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ContentTextBoxWidget(
-                  text: "084.9958A",
-                  width: 120.w,
-                  height: 30.w,
-                  margin: EdgeInsets.all(1.w),
-                ),
-                ContentTextBoxWidget(
-                  text: "084.9958A",
-                  width: 120.w,
-                  height: 30.w,
-                  margin: EdgeInsets.all(1.w),
-                ),
-              ],
-            ),
+            //#. 분양가 목록
+            makePriceListWidget(),
             Gap(17.w)
           ],
         )
     );
   }
 }
+
+/// 분양가 목록 아이템
+class PriceItemWidget extends StatelessWidget {
+  const PriceItemWidget({super.key, this.aptAnnouncementByHouseType});
+  final AptAnnouncementByHouseType? aptAnnouncementByHouseType;
+
+  @override
+  Widget build(BuildContext context) {
+    final String typeText; //#. 공급 규모
+    final String priceText; //#. 분양가
+
+    if(aptAnnouncementByHouseType == null){
+      typeText = APTAnnouncementStrings.couldNotGetData;
+      priceText = APTAnnouncementStrings.couldNotGetData;
+    }
+    else{
+      if(aptAnnouncementByHouseType!.houseType == null){
+        typeText = "알 수 없음";
+        priceText = "알 수 없음";
+      }
+      else{
+        typeText = aptAnnouncementByHouseType!.houseType!;
+
+        if(aptAnnouncementByHouseType!.highestSupplyPrice == null){
+          priceText = "취합 중";
+        }
+        else{
+          priceText = PriceFormatter.tryFormatToEokThousand(
+              double.tryParse(aptAnnouncementByHouseType!.highestSupplyPrice!)
+          ) ?? "알 수 없음";
+        }
+      }
+    }
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ContentTextBoxWidget(
+          text: typeText,
+          width: 120.w,
+          height: 30.w,
+          margin: EdgeInsets.all(1.w),
+          maxLines: 1,
+        ),
+        ContentTextBoxWidget(
+          text: priceText,
+          width: 120.w,
+          height: 30.w,
+          margin: EdgeInsets.all(1.w),
+          maxLines: 1,
+        ),
+      ],
+    );
+  }
+}
+
