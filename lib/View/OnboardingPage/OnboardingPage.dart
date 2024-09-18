875f4b712f717765fd13dfb24d75de1d2b4a70eb
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-09-18 15:44:10 +0900

    feat : 로그인 페이지로 이동 적용 및 두번 눌러 나가기 적용

--- lib/View/OnboardingPage/OnboardingPage.dart
+++ lib/View/OnboardingPage/OnboardingPage.dart
@@ -6,16 +6,23 @@ import 'package:get/get.dart';
 import 'package:homerun/Page/HousingSaleNoticesPage/View/HousingSaleNoticesPage.dart';
 import 'package:homerun/Page/LoginPage/View/LoginPage.dart';
 import 'package:homerun/Style/Fonts.dart';
+import 'package:homerun/View/DubleTapExitWidget.dart';
 
-class OnBoardingPage extends StatelessWidget {
-  OnBoardingPage({super.key}){
-    enterApp();
-  }
+class OnBoardingPage extends StatefulWidget {
+  const OnBoardingPage({super.key});
 
+  @override
+  State<OnBoardingPage> createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
   Future<void> enterApp() async {
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 5)); //몇초로 할지
     if(FirebaseAuth.instance.currentUser == null){
-      Get.to(const LoginPage(goHomeAfterLogin: true,));
+      Get.to(const LoginPage(
+        goHomeAfterLogin: true,
+        hasNoBack: true,
+      ));
     }
     else{
       Get.to(const HousingSaleNoticesPage());
@@ -23,33 +30,41 @@ class OnBoardingPage extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    enterApp();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "청약 정보의 모든것!",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 30.sp,
-                fontFamily: Fonts.BCCard
+    return DoubleTapExitWidget(
+      child: Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "청약 정보의 모든것!",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 30.sp,
+                  fontFamily: Fonts.BCCard
+                ),
               ),
-            ),
-            Gap(30.w),
-            Text(
-              "청약 홈런",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 40.sp,
-                fontFamily: Fonts.BCCard
-              ),
-            )
-          ],
+              Gap(30.w),
+              Text(
+                "청약 홈런",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 40.sp,
+                  fontFamily: Fonts.BCCard
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
