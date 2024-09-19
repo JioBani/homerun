c39b158b587b25aa236773ba227c177582ff3476
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-09-19 15:13:28 +0900

    feat : defaultShadow 수정

--- lib/Style/ShadowPalette.dart
+++ lib/Style/ShadowPalette.dart
@@ -4,10 +4,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ShadowPalette{
   static BoxShadow defaultShadow = BoxShadow(
-    color: Colors.grey.withOpacity(0.5),
-    spreadRadius: 2.w,
-    blurRadius: 5.w,
-    offset: Offset(0, 2.w), // changes position of shadow
+      offset: Offset(0, 2.w),
+      color: Colors.black.withOpacity(0.25),
+      blurRadius: 2.r
   );
 
   static BoxShadow defaultShadowLight = BoxShadow(
