0aabf1736159182124daa9f521fa2fc69629f671
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-10-05 16:29:58 +0900

    detele : 사용하지 않는 서비스 및 파일 제거

--- lib/Service/APTAnnouncementApiService/ProcessedAPTAnnouncementByHouseTypeField.dart
+++
@@ -1,7 +0,0 @@
-class ProcessedAPTAnnouncementByHouseTypeField {
-  static const String totalSpecialSupplyHouseholds = "totalSpecialSupplyHouseholds";
-  static const String totalGeneralSupplyHouseholds = "totalGeneralSupplyHouseholds";
-  static const String maxSupplyPrice = "maxSupplyPrice";
-  static const String minSupplyPrice = "minSupplyPrice";
-  static const String hasSupplyHouseholdsCountError = "hasSupplyHouseholdsCountError";
-}
