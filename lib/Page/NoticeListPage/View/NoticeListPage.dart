7cfcd540132c18807e82b275cad809c83a4072d5
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-10-05 18:24:47 +0900

    add : NoticeListPage

---
+++ lib/Page/NoticeListPage/View/NoticeListPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NoticeListPage extends StatelessWidget {
+  const NoticeListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(),
+    );
+  }
+}
