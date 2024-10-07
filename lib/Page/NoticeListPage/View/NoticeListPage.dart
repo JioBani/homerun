3d3e58bb1818b53af7dfe49850afad3b4f595b23
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-10-07 17:42:34 +0900

    feat : 공고 리스트 페이지 구현

--- lib/Page/NoticeListPage/View/NoticeListPage.dart
+++ lib/Page/NoticeListPage/View/NoticeListPage.dart
@@ -1,12 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:homerun/Common/ApplyHome/SupplyMethod.dart';
+import 'package:homerun/Common/Widget/CustomSearchBar.dart';
+import 'package:homerun/Page/NoticeListPage/Value/SortType.dart';
+import 'package:homerun/Style/Fonts.dart';
+import 'package:homerun/Style/Palette.dart';
 
-class NoticeListPage extends StatelessWidget {
-  const NoticeListPage({super.key});
+import 'NoticePagedListView.dart';
+import 'SortDropDownButtonWidget.dart';
+
+class NoticeListPage extends StatefulWidget {
+  const NoticeListPage({super.key, required this.supplyMethod});
+  final SupplyMethod supplyMethod;
+
+  @override
+  State<NoticeListPage> createState() => _NoticeListPageState();
+}
+
+class _NoticeListPageState extends State<NoticeListPage> {
+  late final Map<SortType, NoticePagedListView> noticePagedListViewMap = {
+    SortType.applicationDate : NoticePagedListView(supplyMethod: widget.supplyMethod, sortType: SortType.applicationDate),
+    SortType.announcementDate : NoticePagedListView(supplyMethod: widget.supplyMethod, sortType: SortType.announcementDate),
+    SortType.popularity : NoticePagedListView(supplyMethod: widget.supplyMethod, sortType: SortType.popularity),
+  };
+
+  SortType sortType = SortType.announcementDate;
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: SortType.values.indexOf(sortType));
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+
+    for (var pagedListView in noticePagedListViewMap.values) {
+      pagedListView.controller.pagingController.dispose();
+    }
+    super.dispose();
+  }
+
+  void _onSortTypeChanged(SortType newSortType) {
+    setState(() {
+      sortType = newSortType;
+    });
+    _pageController.jumpToPage(SortType.values.indexOf(newSortType));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          "분양정보",
+          style: TextStyle(
+              fontSize: 20.sp,
+              fontWeight: FontWeight.bold,
+              color: Palette.defaultSkyBlue,
+              fontFamily: Fonts.title
+          ),
+        ),
+        actions: [
+          SortDropDownButtonWidget(
+            startValue: sortType,
+            onChanged: (value) {
+              _onSortTypeChanged(value);
+            },
+          ),
+          Gap(25.w),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.w),
+        child: Column(
+          children: [
+            Gap(8.w),
+            const CustomSearchBar(),
+            Gap(2.w),
+            Expanded(
+              child: PageView(
+                physics: const NeverScrollableScrollPhysics(),
+                controller: _pageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    sortType = SortType.values[index];
+                  });
+                },
+                children: noticePagedListViewMap.values.toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
