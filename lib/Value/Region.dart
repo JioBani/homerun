d234d07ead9ecc0a973b93adb5f20860ce7549f9
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-10-12 21:03:52 +0900

    feat : 지역 값 수정

--- lib/Value/Region.dart
+++ lib/Value/Region.dart
@@ -1,24 +1,39 @@
 enum Region {
-  seoul('서울'),
-  gyeonggi_incheon('경기·인천'),
-  busan('부산'),
-  sejong_daejeon('세종·대전'),
-  gangwon('강원'),
-  chungbuk_chungnam('충북·충남'),
-  gyeongbuk_gyeongnam('경북·경남'),
-  jeonbuk_jeonnam('전북·전남'),
-  jeju('제주');
+  seoul('서울', 'Seoul'),
+  gyeonggi('경기', 'Gyeonggi'),
+  incheon('인천', 'Incheon'),
+  busan('부산', 'Busan'),
+  daegu('대구', 'Daegu'),
+  gwangju('광주', 'Gwangju'),
+  daejeon_sejong('대전·세종', 'DaejeonSejong'),
+  ulsan('울산', 'Ulsan'),
+  gangwon('강원', 'Gangwon'),
+  chungbuk_chungnam('충북·충남', 'ChungbukChungnam'),
+  jeonbuk_jeonnam('전북·전남', 'JeonbukJeonnam'),
+  gyeongbuk_gyeongnam('경북·경남', 'GyeongbukGyeongnam'),
+  jeju('제주', 'Jeju');
 
-  final String label;
+  final String koreanString;
+  final String englisString;
 
-  const Region(this.label);
+  const Region(this.koreanString, this.englisString);
 
-  static Region? fromString(String label) {
+  static Region? fromKoreanString(String label) {
     for (var value in Region.values) {
-      if (value.label == label) {
+      if (value.koreanString == label) {
         return value;
       }
     }
     return null;
   }
+
+  static List<Region> withoutSeoulGyeonggi(){
+    return Region.values.where((region) => region != Region.seoul && region != Region.gyeonggi).toList();
+  }
+
+  @override
+  String toString() => koreanString;
+
+  // 대문자 카멜케이스로 영어 문자열 반환 함수
+  String toEngString() => englisString;
 }
