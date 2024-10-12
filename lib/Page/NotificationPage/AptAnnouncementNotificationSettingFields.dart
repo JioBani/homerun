c25d17a9bf61c019e277eb62862c5d4fdc4689fe
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-10-12 21:01:12 +0900

    add : AptAnnouncementNotificationSettingFields

---
+++ lib/Page/NotificationPage/AptAnnouncementNotificationSettingFields.dart
@@ -0,0 +1,8 @@
+class AptAnnouncementNotificationSettingFields {
+  static const String houseType = 'HouseType';
+  static const String region = 'Region';
+  static const String seoulAll = 'SeoulAll';
+  static const String seoul = 'Seoul';
+  static const String gyeonggiAll = 'GyeonggiAll';
+  static const String gyeonggi = 'Gyeonggi';
+}
