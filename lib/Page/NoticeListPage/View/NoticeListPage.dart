b489f7a0c707d273c5eab0bb730b9d80e4eae563
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-10-22 18:03:36 +0900

    feat : 검색창에 공급방식 탭 추가

--- lib/Page/NoticeListPage/View/NoticeListPage.dart
+++ lib/Page/NoticeListPage/View/NoticeListPage.dart
@@ -1,12 +1,11 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:homerun/Common/StaticLogger.dart';
 import 'package:homerun/Feature/Notice/Value/HouseType.dart';
 import 'package:homerun/Feature/Notice/Value/SupplyMethod.dart';
-import 'package:homerun/Common/Widget/CustomSearchBar.dart';
 import 'package:homerun/Page/NoticeListPage/Value/SortType.dart';
 import 'package:homerun/Page/NoticeSearchPage/Controller/TagSearchBarController.dart';
 import 'package:homerun/Page/NoticeSearchPage/View/SearchSettingPage.dart';
@@ -26,39 +25,44 @@ class NoticeListPage extends StatefulWidget {
   State<NoticeListPage> createState() => _NoticeListPageState();
 }
 
-class _NoticeListPageState extends State<NoticeListPage> {
-  late final Map<SortType, NoticePagedListView> noticePagedListViewMap = {
-    SortType.applicationDateUpcoming : NoticePagedListView(
-        supplyMethod: widget.supplyMethod,
-        sortType: SortType.applicationDateUpcoming
-    ),
-    SortType.announcementDate : NoticePagedListView(
-        supplyMethod: widget.supplyMethod,
-        sortType: SortType.announcementDate
-    ),
-    SortType.popularity : NoticePagedListView(
-        supplyMethod: widget.supplyMethod,
-        sortType: SortType.popularity)
-    ,
-  };
+class _NoticeListPageState extends State<NoticeListPage> with TickerProviderStateMixin{
+  late final Map<SupplyMethod,Map<SortType, NoticePagedListView>> noticePagedListViewMap;
 
   SortType sortType = SortType.announcementDate;
   late final PageController _pageController;
   final TagSearchBarController tagSearchBarController = TagSearchBarController();
+  late final TabController tabController;
 
   @override
   void initState() {
     super.initState();
     _pageController = PageController(initialPage: SortType.values.indexOf(sortType));
+    tabController = TabController(length: 3, vsync: this);
+
+    noticePagedListViewMap = {};
+
+    for (var method in SupplyMethod.values) {
+      noticePagedListViewMap[method] = {};
+
+      for (var sortType in SortType.values) {
+        noticePagedListViewMap[method]![sortType] = NoticePagedListView(
+          supplyMethod: method,
+          sortType: sortType
+        );
+      }
+    }
   }
 
   @override
   void dispose() {
     _pageController.dispose();
 
-    for (var pagedListView in noticePagedListViewMap.values) {
-      pagedListView.controller.pagingController.dispose();
+    for(var map in noticePagedListViewMap.values){
+      for(var view in map.values){
+        view.controller.pagingController.dispose();
+      }
     }
+
     super.dispose();
   }
 
@@ -104,36 +108,100 @@ class _NoticeListPageState extends State<NoticeListPage> {
                 Map<String,dynamic>? result = await Get.to(SearchSettingPage(tagSearchBarController: tagSearchBarController,));
 
                 if(result != null){
-                  StaticLogger.logger.i("gd");
-                  setState(() {
+                  setState(() { });
 
-                  });
-                  noticePagedListViewMap.values.map((view)=>{
-                    view.controller.setSearch(result['regions'] as List<Region>?, result['houseTypes'] as List<HouseType>)
-                  }).toList();
+                  //#. 순회 하면서 리스트 초기화
+                  for(var map in noticePagedListViewMap.values){
+                    for(var view in map.values){
+                      view.controller.setSearch(result['regions'] as List<Region>?, result['houseTypes'] as List<HouseType>);
+                    }
+                  }
                 }
               },
               child: TagSearchBarPreview(
                 controller: tagSearchBarController,
                 onReset: (){
-                  noticePagedListViewMap.values.map((view)=>{
-                    view.controller.setSearch(null,null)
-                  }).toList();
+                  for(var map in noticePagedListViewMap.values){
+                    for(var view in map.values){
+                      view.controller.setSearch(null, null);
+                    }
+                  }
+
                   tagSearchBarController.reset();
                 },
               )
             ),
             Gap(2.w),
+            Container(
+                decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.grey, width: 0.1.sp))
+                ),
+                child: LayoutBuilder(
+                    builder: (_,constraints) {
+                      double maxWidth = constraints.maxWidth;
+                      return TabBar(
+                          controller: tabController,
+                          tabAlignment: TabAlignment.center,
+                          padding: EdgeInsets.zero,
+                          indicatorPadding: EdgeInsets.zero,
+                          labelPadding: EdgeInsets.zero,
+                          unselectedLabelColor: Colors.grey,
+                          labelStyle: TextStyle(
+                              fontSize: 13.sp,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: Fonts.title
+                          ),
+                          isScrollable: true,
+                          tabs: [
+                            Tab(
+                              child: SizedBox(
+                                width: maxWidth/ 3,
+                                child: Center(
+                                  child: Text(
+                                      "아파트"
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: SizedBox(
+                                width: maxWidth/ 3,
+                                child: Center(
+                                  child: AutoSizeText(
+                                    "무순위/잔여세대",
+                                    maxLines: 1,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: SizedBox(
+                                width: maxWidth/ 3,
+                                child: Center(
+                                  child: Text(
+                                      "임의공급"
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ]
+                      );
+                    }
+                )
+            ),
             Expanded(
-              child: PageView(
-                physics: const NeverScrollableScrollPhysics(),
-                controller: _pageController,
-                onPageChanged: (index) {
-                  setState(() {
-                    sortType = SortType.values[index];
-                  });
-                },
-                children: noticePagedListViewMap.values.toList(),
+              child: TabBarView(
+                controller: tabController,
+                children: SupplyMethod.values.map((method)=> PageView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  controller: _pageController,
+                  onPageChanged: (index) {
+                    setState(() {
+                      sortType = SortType.values[index];
+                    });
+                  },
+                  children: noticePagedListViewMap[method]!.values.toList(),
+                )).toList(),
               ),
             ),
           ],
