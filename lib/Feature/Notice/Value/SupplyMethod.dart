f48112e4679d6075055400c4f863410c9f500025
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-10-25 15:16:48 +0900

    feat : 한글, 영어 enum으로 변경 및 속성, 함수 추가

--- lib/Feature/Notice/Value/SupplyMethod.dart
+++ lib/Feature/Notice/Value/SupplyMethod.dart
@@ -1,8 +1,30 @@
 enum SupplyMethod{
-  General,
-  UnrankedRemain,
-  OptionalSupply
+  General('일반', 'General'),
+
+  UnrankedRemain('무순위/잔여세대', 'UnrankedRemain'),
+
+  OptionalSupply('임의공급', 'OptionalSupply');
+
+  // 한글 및 영어 문자열 값
+  final String koreanName;
+  final String englishName;
+  const SupplyMethod(this.koreanName, this.englishName);
+
+  String toKoreanString() => koreanName;
+
+  // 대문자 카멜케이스로 영어 문자열 반환 함수
+  String toEngString() => englishName;
+
+  static SupplyMethod? fromKoreanString(String label) {
+    for (var value in SupplyMethod.values) {
+      if (value.koreanName == label) {
+        return value;
+      }
+    }
+    return null;
+  }
 }
+
 extension SupplyMethodExtension on SupplyMethod {
   static SupplyMethod fromString(String value) {
     switch (value) {
@@ -26,5 +48,9 @@ extension SupplyMethodExtension on SupplyMethod {
       case SupplyMethod.OptionalSupply:
         return "OptionalSupply";
     }
+  }
+
+  toKoreanString(){
+
   }
 }
